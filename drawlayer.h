97160f4c77f2be1d111061ee47bd3e9800a245e8
John Bowman <bowman@ualberta.ca>
2004-11-18 14:09:11 -0600

    Added layer function.

---
+++ drawlayer.h
@@ -0,0 +1,29 @@
+/*****
+ * drawlayer.h
+ * John Bowman
+ *
+ * Start a new postscript/TeX layer in picture.
+ *****/
+
+#ifndef DRAWLAYER_H
+#define DRAWLAYER_H
+
+#include "drawelement.h"
+
+namespace camp {
+
+class drawLayer : public drawElement {
+public:
+  drawLayer() {}
+
+  virtual ~drawLayer() {}
+
+  bool islayer() {
+    return true;
+  }
+
+};
+
+}
+
+#endif
