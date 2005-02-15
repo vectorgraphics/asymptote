b7b089209392247a58749f86a76e5e5c356589c9
John Bowman <bowman@ualberta.ca>
2005-02-14 20:57:01 -0600

    Added PostScript grestore/gsave objects.

---
+++ drawgrestore.h
@@ -0,0 +1,28 @@
+/*****
+ * drawgrestore.h
+ * John Bowman
+ *
+ * Output PostScript grestore to picture.
+ *****/
+
+#ifndef DRAWGRESTORE_H
+#define DRAWGRESTORE_H
+
+#include "drawelement.h"
+
+namespace camp {
+
+class drawGrestore : public drawElement {
+public:
+  drawGrestore() {}
+  virtual ~drawGrestore() {}
+
+  bool draw(psfile *out) {
+    out->grestore();
+    return true;
+  }
+};
+
+}
+
+#endif
