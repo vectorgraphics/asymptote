52a42f6f9e35ea0f38222059e4ccfdbf66b0fe85
John Bowman <bowman@ualberta.ca>
2005-05-11 03:52:11 -0600

    Define traceable_allocator when garbage collection is disabled.

--- memory.h
+++ memory.h
@@ -14,15 +14,13 @@
 
 #ifdef USEGC
 
-#define ALLOC gc_allocator
-
 #include <gc.h>
 
 #undef GC_MALLOC
 inline void *GC_MALLOC(size_t n) { \
-  void *mem=GC_malloc(n); 	   \
-  if(mem) return mem; 		   \
-  throw std::bad_alloc();	   \
+  void *mem=GC_malloc(n);          \
+  if(mem) return mem;              \
+  throw std::bad_alloc();          \
 }
   
 #include <gc_allocator.h>
@@ -30,8 +28,20 @@ inline void *GC_MALLOC(size_t n) { \
 
 #else // USEGC
 
-#define ALLOC std::allocator
-  
+template<class T>
+class gc_allocator : public std::allocator<T> {
+public:  
+  gc_allocator() {}
+  gc_allocator(const std::allocator<T>&) {}
+};
+
+template<class T>
+class traceable_allocator : public std::allocator<T> {
+public:  
+  traceable_allocator() {}
+  traceable_allocator(const std::allocator<T>&) {}
+};
+
 class gc {};
 
 enum GCPlacement {UseGC,
@@ -50,12 +60,12 @@ inline void* operator new(size_t size, GCPlacement, GCCleanUpFunc=0) {
 
 namespace mem {
 
-#define GC_CONTAINER(KIND)                                              \
-  template <typename T>                                                 \
-  struct KIND : public std::KIND<T, ALLOC<T> > {                        \
-    KIND() : std::KIND<T, ALLOC<T> >() {};                              \
-    KIND(size_t n) : std::KIND<T, ALLOC<T> >(n) {};                     \
-    KIND(size_t n, const T& t) : std::KIND<T, ALLOC<T> >(n,t) {};       \
+#define GC_CONTAINER(KIND)                                               \
+  template <typename T>                                                  \
+  struct KIND : public std::KIND<T, gc_allocator<T> > {                  \
+    KIND() : std::KIND<T, gc_allocator<T> >() {};                        \
+    KIND(size_t n) : std::KIND<T, gc_allocator<T> >(n) {};               \
+    KIND(size_t n, const T& t) : std::KIND<T, gc_allocator<T> >(n,t) {}; \
   }
 
 GC_CONTAINER(list);
@@ -64,10 +74,11 @@ GC_CONTAINER(deque);
 
 #undef GC_CONTAINER
 
-#define GC_CONTAINER(KIND)                                              \
-  template <typename Key, typename T, typename Compare = std::less<Key> > \
-  struct KIND : public std::KIND<Key,T,Compare,ALLOC<std::pair<Key,T> > > { \
-    KIND() : std::KIND<Key,T,Compare,ALLOC<std::pair<Key,T> > > () {}; \
+#define GC_CONTAINER(KIND)                                                    \
+  template <typename Key, typename T, typename Compare = std::less<Key> >     \
+  struct KIND : public                                                        \
+  std::KIND<Key,T,Compare,gc_allocator<std::pair<Key,T> > > {                 \
+    KIND() : std::KIND<Key,T,Compare,gc_allocator<std::pair<Key,T> > > () {}; \
   }
 
 GC_CONTAINER(map);
