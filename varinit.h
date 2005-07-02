712ee95f04440fc4cea116da8859d056a7e7b6e1
Andy Hammerlindl <ahammerlindl@gmail.com>
2005-07-02 09:55:32 -0600

    Excised from exp.h so that dec.h includes less.

---
+++ varinit.h
@@ -0,0 +1,88 @@
+/*****
+ * varinit.h
+ * Andy Hammerlindl 2005/07/01
+ *
+ * Variable initializer are syntax that finish code such as
+ *   int var = ...
+ * As such, they are translated to yield a certain type, the type of the
+ * variable.  Expressions are a special case that can be translated without an
+ * associated variable or its type.
+ *****/
+
+#ifndef VARINIT_H
+#define VARINIT_H
+
+#include "types.h"
+#include "symbol.h"
+#include "absyn.h"
+
+namespace absyntax {
+
+using mem::list;
+using trans::coenv;
+using trans::access;
+using sym::symbol;
+using types::array;
+
+class varinit : public absyn {
+public:
+  varinit(position pos)
+    : absyn(pos) {}
+
+  // This determines what instruction are needed to put the associated
+  // value onto the stack, then adds those instructions to the current
+  // lambda in e.
+  // In some expressions and initializers, the target type needs to be
+  // known in order to translate properly.  For most expressions, this is
+  // kept to a minimum.
+  // For expression, this also allows an implicit cast, hence the name.
+  virtual void transToType(coenv &e, types::ty *target) = 0;
+};
+
+// A default initializer.  For example:
+//   int a;
+// is in some since equivalent to
+//   int a=0;
+// where the definit for int is a function that returns 0.
+class definit : public varinit {
+public:
+  definit(position pos)
+    : varinit(pos) {}
+
+  void prettyprint(ostream &out, int indent);
+
+  void transToType(coenv &e, types::ty *target);
+};
+
+class arrayinit : public varinit {
+  list<varinit *> inits;
+
+  varinit *rest;
+public:
+  arrayinit(position pos)
+    : varinit(pos), rest(0) {}
+
+  virtual ~arrayinit() 
+    {}
+
+  void prettyprint(ostream &out, int indent);
+
+  // Encodes the instructions to make an array from size elements on the stack.
+  static void transMaker(coenv &e, int size, bool rest);
+
+  void transToType(coenv &e, types::ty *target);
+
+  void add(varinit *init) {
+    inits.push_back(init);
+  }
+
+  void addRest(varinit *init) {
+    rest=init;
+  }
+
+  friend class joinExp;
+};
+
+} // namespace absyntax
+
+#endif
