f35731c6d4adab7814eaaed2b450362abbc01c37
John Bowman <bowman@ualberta.ca>
2005-12-14 23:29:34 -0600

    Workaround readline incompatibility under MacOS X 10.4.3.

--- interact.cc
+++ interact.cc
@@ -106,12 +106,10 @@ static const char *inputexpand="erase(); include ";
 static size_t ninput=strlen(input);
 static size_t ninputexpand=strlen(inputexpand);
   
-int readline_startup_hook()
+int MSDOS_startup_hook()
 {
-#ifdef MSDOS
   rl_set_key("\\M-[3~",rl_delete,rl_get_keymap());
   rl_set_key("\\M-[2~",rl_overwrite_mode,rl_get_keymap());
-#endif    
   return 0;
 }
 
@@ -132,7 +130,9 @@ size_t interactive_input(char *buf, size_t max_size)
     
     read_history(historyfile.c_str());
     rl_bind_key('\t',rl_insert); // Turn off tab completion
-    rl_startup_hook=readline_startup_hook;
+#ifdef MSDOS
+    rl_startup_hook=MSDOS_startup_hook;
+#endif    
   }
 
   if(virtualEOF) return 0;
