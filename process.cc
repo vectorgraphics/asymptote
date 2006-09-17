3756452ae55dd4c74dec0832efb762b04501bebc
John Bowman <bowman@ualberta.ca>
2006-09-16 22:18:03 -0600

    Fixed typos in comments.

--- process.cc
+++ process.cc
@@ -387,9 +387,9 @@ class iprompt : public icore {
   };
 
 
-  // The interactive prompt has special functions which cannot be implemented as
+  // The interactive prompt has special functions that cannot be implemented as
   // normal functions.  These special funtions take a commandLine as an argument
-  // and return true if they can handled the command.  If false is return, the
+  // and return true if they can handle the command.  If false is returned, the
   // line is treated as a normal line of code.
   // commands is a map of command names to methods which implement the commands.
   typedef bool (iprompt::*command)(commandLine);
