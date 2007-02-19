8e88b7bece6699bf201b38c3b9014c17b64a9d6a
John Bowman <bowman@ualberta.ca>
2007-02-19 01:42:43 -0600

    Added Philippe's improved show-function-at-point fix.

--- base/asy-mode.el
+++ base/asy-mode.el
@@ -533,8 +533,7 @@ Fields are defined as 'field1: field2.field3:field4' . Field=0 <-> all fields"
     (let ((cWord (current-word))
           (cWindow (selected-window)))
       (switch-to-buffer-other-window "*asy-help*")
-      (kill-buffer nil) 
-      (switch-to-buffer "*asy-help*")
+      (fundamental-mode)
       (setq default-directory "/")
       (if (> emacs-major-version 21)
           (call-process-shell-command
