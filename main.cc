fe42bb6368110b595cf3de0f3ff3dc8005d21061
John Bowman <bowman@ualberta.ca>
2007-07-18 05:09:44 -0600

    Don't call locateFile with an empty file name.

--- main.cc
+++ main.cc
@@ -78,17 +78,19 @@ void interruptHandler(int)
 
 // Run the config file.
 void doConfig(string filename) {
-  string file = locateFile(filename);
-  if(!file.empty()) {
-    bool autoplain=getSetting<bool>("autoplain");
-    bool listvariables=getSetting<bool>("listvariables");
-    if(autoplain) Setting("autoplain")=false; // Turn off for speed.
-    if(listvariables) Setting("listvariables")=false;
+  if(!filename.empty()) {
+    string file = locateFile(filename);
+    if(!file.empty()) {
+      bool autoplain=getSetting<bool>("autoplain");
+      bool listvariables=getSetting<bool>("listvariables");
+      if(autoplain) Setting("autoplain")=false; // Turn off for speed.
+      if(listvariables) Setting("listvariables")=false;
 
-    runFile(file);
+      runFile(file);
 
-    if(autoplain) Setting("autoplain")=true;
-    if(listvariables) Setting("listvariables")=true;
+      if(autoplain) Setting("autoplain")=true;
+      if(listvariables) Setting("listvariables")=true;
+    }
   }
 }
 
