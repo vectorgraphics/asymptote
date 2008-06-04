7049c58edd05e29d05a420e5863c0f813930afc1
John Bowman <bowman@ualberta.ca>
2008-06-03 20:14:07 -0600

    Handle undefined __GNU_C_PREREQ macros.

--- memory.h
+++ memory.h
@@ -15,7 +15,10 @@
 #include <sstream>
 
 #ifndef NOHASH
-#if __GNUC_PREREQ (4,3)
+#ifndef __GNUC_PREREQ
+define __GNUC_PREREQ(maj, min) (0)
+# endif
+#if __GNUC_PREREQ(4,3)
 #include <tr1/unordered_map>
 #define EXT std::tr1
 #else
