21c68e1ee445763474ceb9e79e2d0ced9ece5475
John Bowman <bowman@ualberta.ca>
2008-11-11 17:09:29 -0600

    Fix logic in 1.52-3.

--- glrender.h
+++ glrender.h
@@ -18,7 +18,7 @@
 #ifndef __GNUC_PREREQ
 #define __GNUC_PREREQ(maj, min) (0)
 # endif
-#if !defined(OSG_GLU_TESS_CALLBACK_TRIPLEDOT) || __GNUC_PREREQ(4,3)
+#if !defined(OSG_GLU_TESS_CALLBACK_TRIPLEDOT) && __GNUC_PREREQ(4,3)
 typedef GLvoid (* _GLUfuncptr)();
 #else
 typedef GLvoid (* _GLUfuncptr)(...);
