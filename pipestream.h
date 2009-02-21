b0667031a8755d08adea8132ddb143d538c7b2bb
John Bowman <bowman@ualberta.ca>
2009-02-21 17:03:22 -0600

    Upgrade global license from GPL to LPGL (except as noted within individual files).

--- pipestream.h
+++ pipestream.h
@@ -1,18 +1,18 @@
 /* Pipestream: A simple C++ interface to UNIX pipes
    Version 0.02
-   Copyright (C) 2005-2008 John C. Bowman
+   Copyright (C) 2005-2009 John C. Bowman
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
+   it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. */
 
