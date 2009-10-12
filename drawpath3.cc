1b2b1d9d1de46d07d2cb16ab92ba0dad431985fd
John Bowman <bowman@ualberta.ca>
2009-10-12 10:12:13 -0600

    Don't garbage collect PRC entities.

--- drawpath3.cc
+++ drawpath3.cc
@@ -27,7 +27,7 @@ bool drawPath3::write(prcfile *out, unsigned int *count, array *, array *)
     controls=new(UseGC) Triple[n+1];
     for(Int i=0; i <= n; ++i)
       store(controls[i],g.point(i));
-    out->add(new(UseGC) PRCline(out,n+1,controls,color,scale3D,name.c_str()));
+    out->add(new PRCline(out,n+1,controls,color,scale3D,name.c_str()));
   } else {
     if(name == "")
       buf << "curve-" << count[CURVE]++;
@@ -46,7 +46,7 @@ bool drawPath3::write(prcfile *out, unsigned int *count, array *, array *)
     }
     store(controls[++k],g.precontrol((Int) n));
     store(controls[++k],g.point((Int) n));
-    out->add(new(UseGC) PRCBezierCurve(out,3,m,controls,color,name));
+    out->add(new PRCBezierCurve(out,3,m,controls,color,name));
   }
   return true;
 }
