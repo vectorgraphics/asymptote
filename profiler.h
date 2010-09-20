74e42795dbc169747ae792f951d9db5d679e1028
Andy Hammerlindl <ahammerlindl@gmail.com>
2010-09-20 08:12:23 -0600

    Test for clock_gettime support.

--- profiler.h
+++ profiler.h
@@ -8,7 +8,13 @@
 #ifndef PROFILER_H
 #define PROFILER_H
 
+#include <unistd.h>
+#if _POSIX_TIMERS
+#define HAVE_TIMER
 #include <time.h>
+#endif 
+
+#include <iostream>
 
 #include "inst.h"
 
@@ -46,15 +52,24 @@ class profiler : public gc {
     // It does not include time spent in called function.
     int instructions;
 
+    // Number of instructions spent in this function or its children.  This is
+    // computed by computeTotals.
+    int instTotal;
+
     // The number of real-time nanoseconds spent in this node.  WARNING: May
     // be wildly inaccurate.
-    long nsecs;
+    long long nsecs;
+
+    // Total including children.
+    long long nsecsTotal;
 
     // Call stacks resulting from calls during this call stack.
     mem::vector<node> children;
 
     node(lambda *func)
-      : func(func), calls(0), instructions(0), nsecs(0) {}
+      : func(func), calls(0),
+        instructions(0), instTotal(0),
+        nsecs(0), nsecsTotal(0) {}
 
     // Return the call stack resulting from a call to func when this call
     // stack is current.
@@ -68,6 +83,19 @@ class profiler : public gc {
       children.push_back(node(func));
       return &children.back();
     }
+
+    void computeTotals() {
+      instTotal = 0;
+      nsecsTotal = 0;
+      size_t n = children.size();
+      for (size_t i = 0; i < n; ++i) {
+        children[i].computeTotals();
+        instTotal += children[i].instTotal;
+        nsecsTotal += children[i].nsecsTotal;
+      }
+    }
+
+
 
     void dump(ostream& out) {
 #ifdef DEBUG_FRAME
@@ -104,8 +132,34 @@ class profiler : public gc {
     return *callstack.top();
   }
 
+  // Arc representing one function calling another.  Used only for building
+  // the output for kcachegrind.
+  struct arc : public gc {
+    int calls;
+    int instTotal;
+    long long nsecsTotal;
+
+    arc() : calls(0), instTotal(0), nsecsTotal(0) {}
+
+    void add(node n) {
+      calls += n.calls;
+      instTotal += n.instTotal;
+      nsecsTotal += n.nsecsTotal;
+    }
+  };
+
+  // Representing one function and its calls to other functions.
+  struct fun : public gc {
+    int instructions;
+    long long nsecs;
+    mem::map<lambda *, arc> arcs;
+
+    fun() : instructions(0), nsecs(0) {}
+  };
+
+#ifdef HAVE_TIMER
   // The timestamp when the stack was last changed.  Used to compute the
-  // realtime number of microseconds spent by each function.
+  // realtime number of nanoseconds spent by each function.
   struct timespec timestamp;
 
   static const clockid_t CLOCK_TYPE = CLOCK_REALTIME;
@@ -116,7 +170,7 @@ class profiler : public gc {
 
   // Measure and return the time since the last timestamp, and reset the
   // timestamp to now.
-  long timeAndResetLap() {
+  long long timeAndResetLap() {
     struct timespec now;
     clock_gettime(CLOCK_REALTIME, &now);
     long long nsecs = 10000000000LL * (now.tv_sec - timestamp.tv_sec) +
@@ -124,6 +178,15 @@ class profiler : public gc {
     timestamp = now;
     return nsecs;
   }
+#else
+  // No fast timer support, just return zero for all time intervals.
+  void startLap() {
+    std::cerr << "warning: profiling without timer support" << std::endl;
+  }
+  long long timeAndResetLap() {
+    return 0;
+  }
+#endif
 
 public:
   profiler();
