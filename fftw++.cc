164de7fbfa01109df9bc4124d739acbb2c734976
John Bowman <bowman@ualberta.ca>
2013-02-23 06:26:04 -0600

    Update FFTW++ file.

--- fftw++.cc
+++ fftw++.cc
@@ -1,23 +1,29 @@
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#ifdef HAVE_LIBFFTW3
 #include "fftw++.h"
 
 namespace fftwpp {
 
-std::ifstream fftw::ifWisdom;
-std::ofstream fftw::ofWisdom;
 bool fftw::Wise=false;
+bool fftw::autothreads=true;
 const double fftw::twopi=2.0*acos(-1.0);
-unsigned int fftw::maxthreads=1;
-double fftw::testseconds=0.1; // Time limit for threading efficiency tests
 
 // User settings:
 unsigned int fftw::effort=FFTW_MEASURE;
-const char *fftw::WisdomName=".wisdom";
+const char *WisdomName="wisdom3.txt";
+unsigned int fftw::maxthreads=1;
+double fftw::testseconds=0.1; // Time limit for threading efficiency tests
 
+void fftw::LoadWisdom() {
+  std::ifstream ifWisdom;
+  ifWisdom.open(WisdomName);
+  fftwpp_import_wisdom(GetWisdom,ifWisdom);
+  ifWisdom.close();
 }
 
-#endif
+void fftw::SaveWisdom() {
+  std::ofstream ofWisdom;
+  ofWisdom.open(WisdomName);
+  fftwpp_export_wisdom(PutWisdom,ofWisdom);
+  ofWisdom.close();
+}
+  
+}
