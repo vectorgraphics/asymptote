8554c29a6bd705bd6a974d14ff3bae2051b16394
John Bowman <bowman@ualberta.ca>
2014-04-18 02:04:26 -0600

    Update FFT support file.

--- fftw++.cc
+++ fftw++.cc
@@ -2,15 +2,26 @@
 
 namespace fftwpp {
 
-bool fftw::Wise=false;
-bool fftw::autothreads=true;
 const double fftw::twopi=2.0*acos(-1.0);
+
+fft1d::Table fft1d::threadtable;
+mfft1d::Table mfft1d::threadtable;
+rcfft1d::Table rcfft1d::threadtable;
+crfft1d::Table crfft1d::threadtable;
+mrcfft1d::Table mrcfft1d::threadtable;
+mcrfft1d::Table mcrfft1d::threadtable;
+fft2d::Table fft2d::threadtable;
 
 // User settings:
 unsigned int fftw::effort=FFTW_MEASURE;
-const char *WisdomName=".wisdom";
+const char *fftw::WisdomName=".wisdom";
 unsigned int fftw::maxthreads=1;
-double fftw::testseconds=0.1; // Time limit for threading efficiency tests
+double fftw::testseconds=1.0; // Time limit for threading efficiency tests
+unsigned int fftw::Wise=0;
+bool fftw::mpi=false;
+
+const char *fftw::oddshift="Shift is not implemented for odd nx";
+const char *inout="constructor and call must be both in place or both out of place";
 
 void fftw::LoadWisdom() {
   std::ifstream ifWisdom;
