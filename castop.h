37ad3354d905a5fdde964e43a6306642d764b6c7
John Bowman <bowman@ualberta.ca>
2016-01-30 15:25:32 -0700

    Respect explicit file dimensions.

--- castop.h
+++ castop.h
@@ -120,11 +120,11 @@ void readArray(vm::stack *s, Int nx=-1, Int ny=-1, Int nz=-1)
   camp::file *f = pop<camp::file*>(s);
   vm::array *c=new vm::array(0);
   if(f->isOpen()) {
-    if(nx == -1) nx=f->Nx();
+    if(f->Nx() != -1) nx=f->Nx();
     if(nx == -2) {f->read(nx); f->Nx(-1); if(nx == 0) {s->push(c); return;}}
-    if(ny == -1) ny=f->Ny();
+    if(f->Ny() != -1) ny=f->Ny();
     if(ny == -2) {f->read(ny); f->Ny(-1); if(ny == 0) {s->push(c); return;}}
-    if(nz == -1) nz=f->Nz();
+    if(f->Nz() != -1) nz=f->Nz();
     if(nz == -2) {f->read(nz); f->Nz(-1); if(nz == 0) {s->push(c); return;}}
     T v;
     if(nx >= 0) {
