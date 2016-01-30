9259e447295ead5927e4970a0b894e8eb70702c5
John Bowman <bowman@ualberta.ca>
2016-01-30 16:30:09 -0700

    Fix bug #208 Quotations are broken.

--- fileio.cc
+++ fileio.cc
@@ -115,6 +115,7 @@ void ifile::Read(string& val)
             stream->ignore();
           if(wordmode && !linemode)
             while(isspace(stream->peek())) stream->ignore();
+          if(stream->peek() == '"') {quote=!quote; stream->ignore(); continue;}
           if(s.empty() && c == '\n') {
             stream->ignore();
             continue;
