70059288c59c117db3ce208d834bf153955a5576
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2016-02-04 23:47:31 +0100

    avoid using map (doesn't work in python3)

--- GUI/xasyFile.py
+++ GUI/xasyFile.py
@@ -211,7 +211,7 @@ def parsePathExpression(expr):
     for a in range(len(nodes)):
       if nodes[a] != "cycle":
         nodes[a] = eval(nodes[a])
-    controls = [map(eval,a.replace("controls","").split("and")) for a in tokens if a.startswith("controls")]
+    [eval(a.replace("controls", "").split("and")) for a in tokens if a.startswith("controls")]
     result.initFromControls(nodes, controls)
   else:
     #parse a path without control points
