92c3d17e2a059014ddb13ce8ef50a626f396bfe7
Supakorn Rassameemasmuang <rassamee@ualberta.ca>
2018-06-23 16:10:25 -0600

    Add setup.py file.

---
+++ GUI/setup.py
@@ -0,0 +1,12 @@
+import xasyVersion
+from setuptools import setup
+
+setup(
+    name="xasy",
+    version=xasyVersion.xasyVersion,
+    author="Supakorn Rassameemasmuang, Orest Shardt and John Bowman",
+    author_email="{rassamee, shardt, bowman}@ualberta.ca",
+    description="User Interface for Asymptote, a vector graphics language.", 
+    url="http://asymptote.sourceforge.net",
+    download_url="https://sourceforge.net/projects/asymptote/"
+)
