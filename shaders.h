fdaeeb5d054b9c9faca7718fd5f695c1baeec722
John Bowman <bowman@ualberta.ca>
2019-01-21 06:40:16 -0700

    Port shader to MSDOS.

--- shaders.h
+++ shaders.h
@@ -1,12 +1,18 @@
 #ifndef __TOGL_SHADERSPROC
 #define __TOGL_SHADERSPROC
 
+#define GLEW_STATIC
 #define GLEW_NO_GLU
 
 #ifdef __APPLE__
 #include <OpenGL/glew.h>
 #else
 #include <GL/glew.h>
+#ifdef __MSDOS__
+#include<windows.h>
+#include <GL/wglew.h>
+#include <GL/wglext.h>
+#endif
 #endif
 
 #include <string>
