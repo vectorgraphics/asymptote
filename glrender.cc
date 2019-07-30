37ca40193bf67f729e0d95a0cdf8b7d5747a98e1
John Bowman <bowman@ualberta.ca>
2019-07-30 11:08:43 +1000

    Simplify code.

--- glrender.cc
+++ glrender.cc
@@ -1407,26 +1407,30 @@ void initshader()
   }
   #endif
 
-  camp::materialShader = compileAndLinkShader({
-    ShaderfileModePair(vs.c_str(), GL_VERTEX_SHADER),
-    ShaderfileModePair(fs.c_str(), GL_FRAGMENT_SHADER),
-  }, Nlights, Nmaterials, shaderParams);
-
-  camp::colorShader = compileAndLinkShader({
-    ShaderfileModePair(vs.c_str(), GL_VERTEX_SHADER),
-    ShaderfileModePair(fs.c_str(), GL_FRAGMENT_SHADER),
-  }, Nlights, Nmaterials, shaderParams,true);
-
   camp::noNormalShader = compileAndLinkShader({
     ShaderfileModePair(vs.c_str(), GL_VERTEX_SHADER),
     ShaderfileModePair(fs.c_str(), GL_FRAGMENT_SHADER),
-    },Nlights,Nmaterials,shaderParams,false,false);
+    },Nlights,Nmaterials,shaderParams);
 
   shaderParams.push_back("WIDTH");
   camp::pixelShader = compileAndLinkShader({
     ShaderfileModePair(vs.c_str(), GL_VERTEX_SHADER),
     ShaderfileModePair(fs.c_str(), GL_FRAGMENT_SHADER),
-    },Nlights,Nmaterials,shaderParams,false,false);
+    },Nlights,Nmaterials,shaderParams);
+  shaderParams.pop_back();
+  
+  shaderParams.push_back("NORMAL");
+  camp::materialShader = compileAndLinkShader({
+    ShaderfileModePair(vs.c_str(), GL_VERTEX_SHADER),
+    ShaderfileModePair(fs.c_str(), GL_FRAGMENT_SHADER),
+  }, Nlights, Nmaterials, shaderParams);
+
+  shaderParams.push_back("EXPLICIT_COLOR");
+  camp::colorShader = compileAndLinkShader({
+    ShaderfileModePair(vs.c_str(), GL_VERTEX_SHADER),
+    ShaderfileModePair(fs.c_str(), GL_FRAGMENT_SHADER),
+  }, Nlights, Nmaterials, shaderParams);
+
 }
 
 void deleteshader() 
--- shaders.cc
+++ shaders.cc
@@ -15,16 +15,14 @@
 
 GLuint compileAndLinkShader(std::vector<ShaderfileModePair> const& shaders, 
                             size_t NLights, size_t NMaterials,
-                            std::vector<std::string> const& defineflags,
-                            bool explicitcolor, bool normal)
+                            std::vector<std::string> const& defineflags)
 {
   GLuint mainShader = glCreateProgram();
   std::vector<GLuint> compiledShaders;
 
   for (auto const& shaderInfo : shaders) {
     GLint newshader=createShaderFile(shaderInfo.first,shaderInfo.second,
-                                     NLights,NMaterials,defineflags,
-                                     explicitcolor,normal);
+                                     NLights,NMaterials,defineflags);
     glAttachShader(mainShader,newshader);
     compiledShaders.push_back(newshader);
   }
@@ -72,8 +70,7 @@ GLuint createShaders(GLchar const* src, int shaderType,
 
 GLuint createShaderFile(std::string file, int shaderType, size_t Nlights,
                         size_t Nmaterials,
-                        std::vector<std::string> const& defineflags,
-                        bool explicitcolor, bool normal)
+                        std::vector<std::string> const& defineflags)
 {
   std::ifstream shaderFile;
   shaderFile.open(file.c_str());
@@ -89,12 +86,6 @@ GLuint createShaderFile(std::string file, int shaderType, size_t Nlights,
     shaderSrc << "#define " << flag << "\r\n";
   }
 
-  if(explicitcolor)
-    shaderSrc << "#define EXPLICIT_COLOR" << "\r\n";
-  
-  if(normal)
-    shaderSrc << "#define NORMAL" << "\r\n";
-    
   shaderSrc << "const int Nlights=" << Nlights << ";\r\n";
   shaderSrc << "const int Nmaterials=" << Nmaterials << ";\r\n";
 
