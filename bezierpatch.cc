vectorgraphics/asymptote

commit a05450337791d59966d12fedecb19e73bebc2415

Implement faster WebGL and OpenGL rendering, with improved offscreen detection.

glrender.cc
@@ -29,10 +29,6 @@
 #include "interact.h"
 #include "tr.h"
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #ifdef HAVE_GL
 
 #ifdef HAVE_LIBGLUT
@@ -69,14 +65,30 @@ using camp::nmaterials;
 using utils::seconds;
 
 namespace camp {
+Billboard BB;
+
+GLint pixelShader;
+GLint noNormalShader;
 GLint materialShader;
 GLint colorShader;
-GLint noNormalShader;
-GLint pixelShader;
+GLint transparentShader;
+
+mem::vector<Material> material;
+MaterialMap materialMap;
+size_t materialIndex;
 
 size_t Maxmaterials;
 size_t Nmaterials=1;
 size_t nmaterials=48;
+
+vertexBuffer material0Data;
+vertexBuffer material1Data;
+vertexBuffer materialData;
+vertexBuffer colorData;
+vertexBuffer transparentData;
+
+const size_t Nbuffer=10000;
+const size_t nbuffer=1000;
 }
 
 #endif /* HAVE_GL */
@@ -89,8 +101,6 @@ bool initialize=true;
 
 GLint Maxvertices;
 size_t maxvertices;
-
-size_t Ncenters=100;
 
 using camp::picture;
 using camp::drawRawImage;
@@ -177,10 +187,14 @@ mat4 viewMat;
 mat3 normMat;
 
 dmat4 dprojMat;
+dmat4 dprojViewMat;
 dmat4 dviewMat;
+dmat3 dnormMat=dmat3();
 dmat3 drotateMat; 
 
-ModelView modelView;
+const double *dprojView;
+const double *BBT=value_ptr(dnormMat);
+
 GLuint ubo;
 
 Arcball arcball;
@@ -250,14 +264,12 @@ void frustum(GLdouble left, GLdouble right, GLdouble bottom,
              GLdouble top, GLdouble nearVal, GLdouble farVal)
 {
   dprojMat=glm::frustum(left,right,bottom,top,nearVal,farVal);
-  projViewMat=mat4(dprojMat*dviewMat);
 }
 
 void ortho(GLdouble left, GLdouble right, GLdouble bottom,
            GLdouble top, GLdouble nearVal, GLdouble farVal)
 {
   dprojMat=glm::ortho(left,right,bottom,top,nearVal,farVal);
-  projViewMat=mat4(dprojMat*dviewMat);
 }
 
 void setProjection()
@@ -265,6 +277,9 @@ void setProjection()
   setDimensions(Width,Height,X,Y);
   if(orthographic) ortho(xmin,xmax,ymin,ymax,-zmax,-zmin);
   else frustum(xmin,xmax,ymin,ymax,-zmax,-zmin);
+  dprojViewMat=dprojMat*dviewMat;
+  projViewMat=mat4(dprojViewMat);
+  dprojView=value_ptr(dprojViewMat);
   
 #ifdef HAVE_GL
 #ifdef HAVE_LIBGLUT
@@ -278,18 +293,10 @@ void setProjection()
 void updateModelViewData()
 {
   // Like Fortran, OpenGL uses transposed (column-major) format!
-  dmat4 MV=glm::transpose(dviewMat);
-  dmat4 MVinv=glm::inverse(MV);
-  
-  normMat=mat3(MVinv);
-
-  double* T=value_ptr(MV);
-  double* Tinv=value_ptr(MVinv);
-
-  for(int j=0; j < 16; ++j) {
-    modelView.T[j]=T[j];
-    modelView.Tinv[j]=Tinv[j];
-  }
+  dmat4 MVinv=glm::inverse(dviewMat);
+  dnormMat=dmat3(MVinv);
+  BBT=value_ptr(dnormMat);
+  normMat=mat3(dnormMat);
 }
 
 bool Xspin,Yspin,Zspin;
@@ -435,14 +442,10 @@ void drawscene(int Width, int Height)
     forceRemesh=false;
   }
   
-  if(remesh) {
-    camp::BezierPatch::clear();
-    camp::BezierPatch::Clear();
-    camp::BezierPatch::tClear();
-    camp::BezierCurve::clear();
-    camp::Pixel::clear();
+  camp::clearBuffers();
+  
+  if(remesh)
     camp::drawElement::center.clear();
-  }
   
   // Render opaque objects
   Picture->render(size2,m,M,perspective,false,remesh);
@@ -1189,11 +1192,6 @@ void spinz()
   }
 }
 
-void write(const char *text, const double *v)
-{
-  cout << text << "=(" << v[0] << "," << v[1] << "," << v[2] << ")";
-}
-
 void showCamera()
 {
   projection P=camera();
@@ -1410,15 +1408,10 @@ void init_osmesa()
   }
 #endif // HAVE_LIBOSMESA
 }
-
-GLuint vertShader,fragShader;
-GLuint vertShaderCol,fragShaderCol;
-
 void initshader()
 {
   Nlights=max(Nlights,nlights);
   Nmaterials=max(Nmaterials,nmaterials);
-  Ncenters=max(Ncenters,camp::drawElement::center.size());
   shaderProg=glCreateProgram();
   string vs=locateFile("shaders/vertex.glsl");
   string fs=locateFile("shaders/fragment.glsl");
@@ -1442,30 +1435,29 @@ void initshader()
   if(orthographic)
     shaderParams.push_back("ORTHOGRAPHIC");
     
-  shaderParams.push_back("BILLBOARD");
-  camp::noNormalShader=compileAndLinkShader(shaders,Nlights,Nmaterials,
-                                            Ncenters,shaderParams);
-  shaderParams.pop_back();
-
   shaderParams.push_back("WIDTH");
   camp::pixelShader=compileAndLinkShader(shaders,Nlights,Nmaterials,
-                                         Ncenters,shaderParams);
+                                         shaderParams);
   shaderParams.pop_back();
-  
+  camp::noNormalShader=compileAndLinkShader(shaders,Nlights,Nmaterials,
+                                            shaderParams);
   shaderParams.push_back("NORMAL");
   camp::materialShader=compileAndLinkShader(shaders,Nlights,Nmaterials,
-                                            Ncenters,shaderParams);
-
-  shaderParams.push_back("EXPLICIT_COLOR");
-  shaderParams.push_back("BILLBOARD");
+                                            shaderParams);
+  shaderParams.push_back("COLOR");
   camp::colorShader=compileAndLinkShader(shaders,Nlights,Nmaterials,
-                                         Ncenters,shaderParams);
+                                         shaderParams);
+  shaderParams.push_back("TRANSPARENT");
+  camp::transparentShader=compileAndLinkShader(shaders,Nlights,Nmaterials,
+                                               shaderParams);
 }
 
 void deleteshader() 
 {
-  glDeleteProgram(camp::materialShader);
+  glDeleteProgram(camp::transparentShader);
   glDeleteProgram(camp::colorShader);
+  glDeleteProgram(camp::materialShader);
+  glDeleteProgram(camp::pixelShader);
   glDeleteProgram(camp::noNormalShader);
 }
 
@@ -1792,15 +1784,13 @@ string getCenterIndex(size_t const& index) {
 
 void setUniforms(GLint shader)
 {
-  if(gl::nlights > gl::Nlights || nmaterials > Nmaterials || 
-     drawElement::center.size() > gl::Ncenters) {
+  if(gl::nlights > gl::Nlights || nmaterials > Nmaterials) {
     gl::deleteshader();
     gl::initshader();
     gl::lastshader=-1;
   }
   
-  bool billboard=shader == colorShader || shader == noNormalShader;
-  bool normal=shader == colorShader || shader == materialShader;
+  bool normal=shader != pixelShader && shader != noNormalShader;
     
   if(shader != gl::lastshader) {
     glUseProgram(shader);
@@ -1822,15 +1812,6 @@ void setUniforms(GLint shader)
                   (GLfloat) gl::Diffuse[i4+2]);
     }
 
-    if(billboard) {
-      size_t ncenters=drawElement::center.size();
-      for(size_t i=0; i < ncenters; ++i) {
-        triple v=drawElement::center[i];
-        glUniform3f(glGetUniformLocation(shader,getCenterIndex(i).c_str()),
-                    (GLfloat) v.getx(),(GLfloat) v.gety(),(GLfloat) v.getz());
-      }
-    }
-  
 #if HAVE_LIBOPENIMAGEIO
     // textures
     if (settings::getSetting<bool>("envmap")) { 
@@ -1849,8 +1830,8 @@ void setUniforms(GLint shader)
   glGenBuffers(1,&gl::ubo);
   glBindBuffer(GL_UNIFORM_BUFFER,gl::ubo);
     
-  glBufferData(GL_UNIFORM_BUFFER,drawElement::material.size()*sizeof(Material),
-               drawElement::material.data(),GL_STATIC_DRAW);
+  glBufferData(GL_UNIFORM_BUFFER,material.size()*sizeof(Material),
+               material.data(),GL_STATIC_DRAW);
   glBindBufferBase(GL_UNIFORM_BUFFER,binding,gl::ubo);
   
   glUniformMatrix4fv(glGetUniformLocation(shader,"projViewMat"),1,GL_FALSE, value_ptr(gl::projViewMat));
@@ -1867,6 +1848,128 @@ void deleteUniforms()
   glDeleteBuffers(1,&gl::ubo);
 }
   
+void drawBuffer(vertexBuffer& data, GLint shader)
+{
+  if(data.indices.empty()) return;
+  
+  bool pixel=shader == pixelShader;
+  bool normal=!pixel && (shader != noNormalShader);
+  bool color=shader == colorShader || shader == transparentShader;
+  
+  const size_t size=sizeof(GLfloat);
+  const size_t intsize=sizeof(GLint);
+  const size_t bytestride=color ? sizeof(VertexData) :
+    (normal ? sizeof(vertexData) :
+     (pixel ? sizeof(vertexData0) : sizeof(vertexData1)));
+
+  GLuint vertsBufferIndex;
+  GLuint elemBufferIndex; 
+
+  GLuint vao;
+  
+  glGenVertexArrays(1,&vao);
+  glBindVertexArray(vao);
+
+  glGenBuffers(1,&vertsBufferIndex);
+  glGenBuffers(1,&elemBufferIndex);
+  
+  if(color) registerBuffer(data.Vertices,vertsBufferIndex);
+  else if(normal) registerBuffer(data.vertices,vertsBufferIndex);
+  else if(pixel) registerBuffer(data.vertices0,vertsBufferIndex);
+  else registerBuffer(data.vertices1,vertsBufferIndex);
+  
+  registerBuffer(data.indices,elemBufferIndex);
+  
+  glBindBuffer(GL_ARRAY_BUFFER,vertsBufferIndex);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,elemBufferIndex);
+
+  camp::setUniforms(shader);
+
+  const GLint posAttrib=glGetAttribLocation(shader,"position");
+  const GLint materialAttrib=glGetAttribLocation(shader,"material");
+  GLint normalAttrib,colorAttrib,widthAttrib=0;
+  
+  glVertexAttribPointer(posAttrib,3,GL_FLOAT,GL_FALSE,bytestride,(void *) 0);
+  glEnableVertexAttribArray(posAttrib);
+    
+  if(normal) {
+    normalAttrib=glGetAttribLocation(shader,"normal");
+    glVertexAttribPointer(normalAttrib,3,GL_FLOAT,GL_FALSE,bytestride,
+                          (void *) (3*size));
+    glEnableVertexAttribArray(normalAttrib);
+  } else if(pixel) {
+    widthAttrib=glGetAttribLocation(shader,"width");
+    glVertexAttribPointer(widthAttrib,1,GL_FLOAT,GL_FALSE,bytestride,
+                          (void *) (3*size));
+    glEnableVertexAttribArray(widthAttrib);
+  }
+    
+  glVertexAttribIPointer(materialAttrib,1,GL_INT,bytestride, 
+                         (void *) ((normal ? 6 : (pixel ? 4 : 3))*size));
+  glEnableVertexAttribArray(materialAttrib);
+
+  if(color) {
+    colorAttrib=glGetAttribLocation(shader,"color");
+    glVertexAttribPointer(colorAttrib,4,GL_UNSIGNED_BYTE,GL_TRUE,bytestride,
+                          (void *) (6*size+intsize));
+    glEnableVertexAttribArray(colorAttrib);
+  }
+  
+  glFlush(); // Workaround broken MSWindows drivers for Intel GPU
+  glDrawElements(normal ? GL_TRIANGLES : (pixel ? GL_POINTS : GL_LINES),
+                 data.indices.size(),GL_UNSIGNED_INT,(void *) 0);
+
+  glDisableVertexAttribArray(posAttrib);
+  if(normal)
+   glDisableVertexAttribArray(normalAttrib);
+  if(pixel)
+   glDisableVertexAttribArray(widthAttrib);
+  glDisableVertexAttribArray(materialAttrib);
+  if(color)
+    glDisableVertexAttribArray(colorAttrib);
+  
+  deleteUniforms();
+  
+  glBindBuffer(GL_ARRAY_BUFFER,0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
+
+  glBindVertexArray(0);
+  glDeleteVertexArrays(1,&vao);
+  
+  glDeleteBuffers(1,&vertsBufferIndex);
+  glDeleteBuffers(1,&elemBufferIndex);
+}
+
+void drawBuffers() 
+{
+  drawBuffer(material0Data,pixelShader);
+  drawBuffer(material1Data,noNormalShader);
+  drawBuffer(materialData,materialShader);
+  drawBuffer(colorData,colorShader);
+  sortTriangles();
+  drawBuffer(transparentData,transparentShader);
+}
+
+void clearBuffers()
+{
+  material0Data.clear();
+  material1Data.clear();
+  materialData.clear();
+  colorData.clear();
+  transparentData.clear();
+}
+
+void clearMaterialBuffer(bool draw)
+{
+  if(draw)
+    drawBuffers();
+  material.clear();
+  material.reserve(nmaterials);
+  materialMap.clear();
+  materialIndex=0;
+}
+
+
 }
 
 #endif /* HAVE_GL */

glrender.h
@@ -9,6 +9,12 @@
 #include "common.h"
 #include "triple.h"
 
+#ifdef HAVE_LIBGLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#endif
+
 #ifdef HAVE_GL
 
 #include <csignal>
@@ -50,6 +56,8 @@
 #else
 typedef float GLfloat;
 #endif
+
+#include "material.h"
 
 namespace camp {
 class picture;
@@ -131,27 +139,233 @@ void glrender(const string& prefix, const camp::picture* pic,
               size_t nlights, camp::triple *lights, double *diffuse,
               double *specular, bool view, int oldpid=0);
 
-struct ModelView {
-  double T[16];
-  double Tinv[16];
-};
-
-extern ModelView modelView;
+extern const double *dprojView;
 
 void initshader();
 void deleteshader();
+
+extern const double *BBT;
 
 }
 
 namespace camp {
 
+struct Billboard {
+  double cx,cy,cz;
+  
+  void init(const triple& center) {
+    cx=center.getx();
+    cy=center.gety();
+    cz=center.getz();
+  }
+    
+  triple transform(const triple& v) const {
+    double x=v.getx()-cx;
+    double y=v.gety()-cy;
+    double z=v.getz()-cz;
+    
+    return triple(x*gl::BBT[0]+y*gl::BBT[3]+z*gl::BBT[6]+cx,
+                  x*gl::BBT[1]+y*gl::BBT[4]+z*gl::BBT[7]+cy,
+                  x*gl::BBT[2]+y*gl::BBT[5]+z*gl::BBT[8]+cz);
+  }
+};
+
+extern Billboard BB;
+
+extern GLint pixelShader;
+extern GLint noNormalShader;
 extern GLint materialShader;
 extern GLint colorShader;
-extern GLint noNormalShader;
-extern GLint pixelShader;
+extern GLint transparentShader;
 
 void setUniforms(GLint shader);
 void deleteUniforms();
+
+typedef mem::map<CONST Material,size_t> MaterialMap;
+
+extern mem::vector<Material> material;
+extern MaterialMap materialMap;
+extern size_t materialIndex;
+extern int MaterialIndex;
+
+extern const size_t Nbuffer; // Initial size of 2D dynamic buffers
+extern const size_t nbuffer; // Initial size of 0D & 1D dynamic buffers
+
+class vertexData 
+{
+public:
+  GLfloat position[3];
+  GLfloat normal[3];
+  GLint material;
+  vertexData() {};
+  vertexData(const triple& v, const triple& n) {
+    position[0]=v.getx();
+    position[1]=v.gety();
+    position[2]=v.getz();
+    normal[0]=n.getx();
+    normal[1]=n.gety();
+    normal[2]=n.getz();
+    material=MaterialIndex;
+  }
+};
+
+class VertexData
+{
+public:
+  GLfloat position[3];
+  GLfloat normal[3];
+  GLint material;
+  GLubyte color[4];
+  VertexData() {};
+  VertexData(const triple& v, const triple& n) {
+    position[0]=v.getx();
+    position[1]=v.gety();
+    position[2]=v.getz();
+    normal[0]=n.getx();
+    normal[1]=n.gety();
+    normal[2]=n.getz();
+    material=MaterialIndex;
+  }
+  VertexData(const triple& v, const triple& n, GLfloat *c) {
+    position[0]=v.getx();
+    position[1]=v.gety();
+    position[2]=v.getz();
+    normal[0]=n.getx();
+    normal[1]=n.gety();
+    normal[2]=n.getz();
+    material=MaterialIndex;
+    color[0]=(int)(bytescale*c[0]);
+    color[1]=(int)(bytescale*c[1]);
+    color[2]=(int)(bytescale*c[2]);
+    color[3]=(int)(bytescale*c[3]);
+  }
+};
+
+class vertexData1 {
+public:
+  GLfloat position[3];
+  GLint material;
+  vertexData1() {};
+  vertexData1(const triple& v) {
+    position[0]=v.getx();
+    position[1]=v.gety();
+    position[2]=v.getz();
+    material=MaterialIndex;
+  }
+};
+
+class vertexData0 {
+public:
+  GLfloat position[3];
+  GLfloat width;
+  GLint  material;
+  vertexData0() {};
+  vertexData0(const triple& v, double width) : width(width) {
+    position[0]=v.getx();
+    position[1]=v.gety();
+    position[2]=v.getz();
+    material=MaterialIndex;
+  }
+};
+
+class vertexBuffer {
+public:  
+  std::vector<vertexData> vertices;
+  std::vector<VertexData> Vertices;
+  std::vector<vertexData1> vertices1;
+  std::vector<vertexData0> vertices0;
+  std::vector<GLuint> indices;
+  void clear() {
+    vertices.clear();
+    Vertices.clear();
+    vertices1.clear();
+    vertices0.clear();
+    indices.clear();
+    vertices.reserve(Nbuffer);
+    Vertices.reserve(Nbuffer);
+    vertices1.reserve(nbuffer);
+    vertices0.reserve(nbuffer);
+    indices.reserve(Nbuffer);
+  }
+  
+// Store the vertex v and its normal vector n.
+  GLuint vertex(const triple &v, const triple& n) {
+    size_t nvertices=vertices.size();
+    vertices.push_back(vertexData(v,n));
+    return nvertices;
+  }     
+  
+// Store the vertex v and its normal vector n, without an explicit color.
+  GLuint tvertex(const triple &v, const triple& n) {
+    size_t nvertices=Vertices.size();
+    Vertices.push_back(VertexData(v,n));
+    return nvertices;
+  }
+  
+// Store the vertex v, its normal vector n, and colors c.
+  GLuint Vertex(const triple &v, const triple& n, GLfloat *c) {
+    size_t nvertices=Vertices.size();
+    Vertices.push_back(VertexData(v,n,c));
+    return nvertices;
+  }     
+  
+// Store the vertex v.
+  GLuint vertex1(const triple &v) {
+    size_t nvertices=vertices1.size();
+    vertices1.push_back(vertexData1(v));
+    return nvertices;
+  }     
+  
+// Store the pixel v and its width.
+  GLuint vertex0(const triple &v, double width) {
+    size_t nvertices=vertices0.size();
+    vertices0.push_back(vertexData0(v,width));
+    return nvertices;
+  }     
+  
+  // append array b onto array a with offset
+  void appendOffset(std::vector<GLuint>& a,
+                    const std::vector<GLuint>& b, size_t offset) {
+    size_t n=a.size();
+    size_t m=b.size();
+    a.resize(n+m);
+    for(size_t i=0; i < m; ++i)
+      a[n+i]=b[i]+offset;
+  }
+
+  // append array b onto array a
+  void append(const vertexBuffer& b) {
+    appendOffset(indices,b.indices,vertices.size());
+    vertices.insert(vertices.end(),b.vertices.begin(),b.vertices.end());
+  }
+  
+  void Append(const vertexBuffer& b) {
+    appendOffset(indices,b.indices,Vertices.size());
+    Vertices.insert(Vertices.end(),b.Vertices.begin(),b.Vertices.end());
+  }
+  
+  void append1(const vertexBuffer& b) {
+    appendOffset(indices,b.indices,vertices1.size());
+    vertices1.insert(vertices1.end(),b.vertices1.begin(),b.vertices1.end());
+  }
+  
+  void append0(const vertexBuffer& b) {
+    appendOffset(indices,b.indices,vertices0.size());
+    vertices0.insert(vertices0.end(),b.vertices0.begin(),b.vertices0.end());
+  }
+
+};
+
+extern vertexBuffer material0Data; // pixels
+extern vertexBuffer material1Data; // Material Bezier curves
+extern vertexBuffer materialData;  // Material Bezier patches & triangles
+extern vertexBuffer colorData;     // Colored Bezier patches & triangles
+extern vertexBuffer transparentData;//Transparent patches & triangles
+
+extern void drawBuffer(vertexBuffer& data, GLint shader);
+extern void drawBuffers(); 
+extern void clearBuffers();
+extern void clearMaterialBuffer(bool draw=false);
 
 }
 

base/shaders/fragment.glsl
@@ -25,7 +25,7 @@ in vec3 Normal;
 vec3 normal;
 #endif
 
-#ifdef EXPLICIT_COLOR
+#ifdef COLOR
 in vec4 Color; 
 #endif
 
@@ -134,31 +134,31 @@ void main()
   vec4 parameters;
 
   Material m;
-#ifdef EXPLICIT_COLOR
-  if(materialIndex < 0) {
-    int index=-materialIndex-1;
-    m=Materials[index];
-    diffuse=Color;
-    emissive=vec4(0.0);
-  } else {
-    m=Materials[materialIndex];
+#ifdef TRANSPARENT
+  m=Materials[abs(materialIndex)-1];
+  if(materialIndex >= 0) {
     diffuse=m.diffuse;
     emissive=m.emissive;
+  } else {
+    diffuse=Color;
+    emissive=vec4(0.0);
   }
 #else
-  m=Materials[materialIndex];
+  m=Materials[int(materialIndex)];
+#ifdef COLOR
+  diffuse=Color;
+  emissive=vec4(0.0);
+#else  
   diffuse=m.diffuse; 
   emissive=m.emissive;
 #endif
+#endif
   Specular=m.specular.rgb;
   parameters=m.parameters;
-
   Roughness2=1.0-parameters[0];
   Roughness2=Roughness2*Roughness2;
-  
   Metallic=parameters[1];
   Fresnel0=parameters[2];
-
   Diffuse=diffuse.rgb;
 
   // Given a point x and direction \omega,
@@ -186,7 +186,7 @@ void main()
     }
 
 #ifdef ENABLE_TEXTURE
-#ifndef EXPLICIT_COLOR
+#ifndef COLOR
     // Experimental environment radiance using Riemann sums;
     // can also do importance sampling.
     vec3 envRadiance=vec3(0.0,0.0,0.0);

base/shaders/vertex.glsl
@@ -1,19 +1,18 @@
 in vec3 position;
 
+uniform mat3 normMat;
+
 #ifdef NORMAL
 #ifndef ORTHOGRAPHIC
 out vec3 ViewPosition;
 #endif
 in vec3 normal;
 out vec3 Normal;
-uniform mat3 normMat;
-#else
-#ifdef BILLBOARD
-uniform mat3 normMat;
-#endif
 #endif
 
-#ifdef EXPLICIT_COLOR
+in int material;
+
+#ifdef COLOR
 in vec4 color;
 out vec4 Color;
 #endif
@@ -22,36 +21,23 @@ out vec4 Color;
 in float width;
 #endif
 
-in int material;
-
 uniform mat4 projViewMat;
 uniform mat4 viewMat;
-
-#ifdef BILLBOARD
-in int centerIndex;
-uniform vec3 Centers[Ncenter];
-#endif
 
 flat out int materialIndex;
 
 void main()
 {
-#ifdef BILLBOARD
-  int index=int(centerIndex);
-  vec4 v=vec4(index == 0 ? position :
-    Centers[index-1]+(position-Centers[index-1])*normMat,1.0);
-#else    
   vec4 v=vec4(position,1.0);
-#endif
   gl_Position=projViewMat*v;
 #ifdef NORMAL
 #ifndef ORTHOGRAPHIC
   ViewPosition=(viewMat*v).xyz;
 #endif
-  Normal=normMat*normal;
+  Normal=normal*normMat;
 #endif
 
-#ifdef EXPLICIT_COLOR
+#ifdef COLOR
   Color=color;
 #endif
 

bbox3.h
@@ -178,48 +178,6 @@ struct bbox3 {
     return pair(right,top);
   }
   
-  // transform bbox3 by 4x4 matrix
-  void transform(const double* m)
-  {
-    const double xmin = left;
-    const double ymin = bottom;
-    const double zmin = near;
-    const double xmax = right;
-    const double ymax = top;
-    const double zmax = far;
-    
-    empty = true;
-    add(m*triple(xmin,ymin,zmin));
-    addnonempty(m*triple(xmin,ymin,zmax));
-    addnonempty(m*triple(xmin,ymax,zmin));
-    addnonempty(m*triple(xmin,ymax,zmax));
-    addnonempty(m*triple(xmax,ymin,zmin));
-    addnonempty(m*triple(xmax,ymin,zmax));
-    addnonempty(m*triple(xmax,ymax,zmin));
-    addnonempty(m*triple(xmax,ymax,zmax));
-  }
-  
-  void transform2(const double* m)
-  {
-    const double xmin = left;
-    const double ymin = bottom;
-    const double zmin = near;
-    const double xmax = right;
-    const double ymax = top;
-    const double zmax = far;
-    
-    pair V=Transform2(m,triple(xmin,ymin,zmin));
-    left=right=V.getx();
-    bottom=top=V.gety();
-    addnonempty(Transform2(m,triple(xmin,ymin,zmax)));
-    addnonempty(Transform2(m,triple(xmin,ymax,zmin)));
-    addnonempty(Transform2(m,triple(xmin,ymax,zmax)));
-    addnonempty(Transform2(m,triple(xmax,ymin,zmin)));
-    addnonempty(Transform2(m,triple(xmax,ymin,zmax)));
-    addnonempty(Transform2(m,triple(xmax,ymax,zmin)));
-    addnonempty(Transform2(m,triple(xmax,ymax,zmax)));
-  }
-  
   friend ostream& operator << (ostream& out, const bbox3& b)
   {
     out << "Min " << b.Min() << " Max " << b.Max();

beziercurve.cc
@@ -12,34 +12,28 @@ namespace camp {
 
 #ifdef HAVE_LIBGLM
 
-std::vector<vertexData1> BezierCurve::vertexbuffer;
-std::vector<GLuint> BezierCurve::indices;
-std::vector<pixelData> Pixel::vertexbuffer;
-
-GLuint BezierCurve::vertsBufferIndex;
-GLuint BezierCurve::elemBufferIndex;
-
-void BezierCurve::init(double res, const pair& Min, const pair& Max,
-                       bool billboard)
+void BezierCurve::init(double res)
 {
-  const size_t nbuffer=1000; // Initial size of dynamic buffers
-
-  Offscreen=false;
-  vertexbuffer.reserve(nbuffer);
-  indices.reserve(nbuffer);
-  pindices=&indices;
-  
-  if(billboard)
-    pvertex=&bvertex;
-  else
-    pvertex=&vertex;
-  
   this->res=res;
   res2=res*res;
-  this->Min=Min;
-  this->Max=Max;
+  
+  MaterialIndex=materialIndex;
 }
 
+void BezierCurve::render(const triple *p, bool straight) 
+{
+  GLuint i0=data.vertex1(p[0]);
+  GLuint i3=data.vertex1(p[3]);
+    
+  if(straight) {
+    std::vector<GLuint> &q=data.indices;
+    q.push_back(i0);
+    q.push_back(i3);
+  } else
+    render(p,i0,i3);
+  append();
+}
+  
 // Use a uniform partition to draw a Bezier patch.
 // p is an array of 4 triples representing the control points.
 // Ii are the vertices indices.
@@ -52,8 +46,9 @@ void BezierCurve::render(const triple *p, GLuint I0, GLuint I1)
   if(Straightness(p0,p1,p2,p3) < res2) { // Segment is flat
     triple P[]={p0,p3};
     if(!offscreen(2,P)) {
-      indices.push_back(I0);
-      indices.push_back(I1);
+      std::vector<GLuint> &q=data.indices;
+      q.push_back(I0);
+      q.push_back(I1);
     }
   } else { // Segment is not flat
     if(offscreen(4,p)) return;
@@ -67,133 +62,11 @@ void BezierCurve::render(const triple *p, GLuint I0, GLuint I1)
     triple s0[]={p0,m0,m3,m5};
     triple s1[]={m5,m4,m2,p3};
       
-    GLuint i0=pvertex(m5);
+    GLuint i0=data.vertex1(m5);
       
     render(s0,I0,i0);
     render(s1,i0,I1);
   }
-}
-
-void BezierCurve::render(const triple *p, bool straight) 
-{
-  GLuint i0=pvertex(p[0]);
-  GLuint i3=pvertex(p[3]);
-    
-  if(straight) {
-    indices.push_back(i0);
-    indices.push_back(i3);
-  } else
-    render(p,i0,i3);
-}
-  
-void BezierCurve::draw()
-{
-  if(indices.empty()) return;
-  
-  const size_t size=sizeof(GLfloat);
-  const size_t intsize=sizeof(GLint);
-  const size_t bytestride=sizeof(vertexData1);
-
-  GLuint vao;
-  
-  glGenVertexArrays(1,&vao);
-  glBindVertexArray(vao);
-    
-  glGenBuffers(1,&vertsBufferIndex);
-  glGenBuffers(1,&elemBufferIndex);
-
-  registerBuffer(vertexbuffer,vertsBufferIndex);
-  registerBuffer(indices,elemBufferIndex);
-    
-  glBindBuffer(GL_ARRAY_BUFFER,vertsBufferIndex);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,elemBufferIndex);
-
-  camp::setUniforms(noNormalShader);
-  
-  const GLint posAttrib=glGetAttribLocation(noNormalShader, "position");
-  const GLint materialAttrib=glGetAttribLocation(noNormalShader,"material");
-  const GLint centerAttrib=glGetAttribLocation(noNormalShader,"centerIndex");
-
-  glVertexAttribPointer(posAttrib,3,GL_FLOAT,GL_FALSE,bytestride,(void *) 0);
-  glEnableVertexAttribArray(posAttrib);
-
-  glVertexAttribIPointer(materialAttrib,1,GL_INT,bytestride,(void *) (3*size));
-  glEnableVertexAttribArray(materialAttrib);
-
-  glVertexAttribIPointer(centerAttrib,1,GL_INT,bytestride,
-                         (void *) (3*size+intsize));
-  glEnableVertexAttribArray(centerAttrib);
-  
-  glFlush(); // Workaround broken MSWindows drivers for Intel GPU
-  glDrawElements(GL_LINES,indices.size(),GL_UNSIGNED_INT,(void*)(0));
-  
-  glDisableVertexAttribArray(posAttrib);
-  glDisableVertexAttribArray(materialAttrib);
-  glDisableVertexAttribArray(centerAttrib);
-  
-  deleteUniforms();
-  
-  glBindBuffer(GL_ARRAY_BUFFER,0);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
-
-  glBindVertexArray(0);
-  glDeleteVertexArrays(1,&vao);
-  
-  glDeleteBuffers(1,&vertsBufferIndex);
-  glDeleteBuffers(1,&elemBufferIndex);
-}
-
-void Pixel::queue(const triple& p, double width)
-{
-  vertex(p,width);
-}
-
-void Pixel::draw()
-{
-  if(vertexbuffer.size() == 0)
-    return;
-
-  const size_t size=sizeof(GLfloat);
-  const size_t bytestride=sizeof(pixelData);
-
-  GLuint vbo;
-  glGenBuffers(1,&vbo);
-  
-  GLuint vao;
-  glGenVertexArrays(1,&vao);
-  glBindVertexArray(vao);
-
-  glBindBuffer(GL_ARRAY_BUFFER,vbo);
-  glBufferData(GL_ARRAY_BUFFER,bytestride*vertexbuffer.size(),
-               vertexbuffer.data(),GL_STATIC_DRAW);
-
-  camp::setUniforms(pixelShader); 
-  
-  const GLint posAttrib=glGetAttribLocation(pixelShader, "position");
-  const GLint materialAttrib=glGetAttribLocation(pixelShader,"material");
-  const GLint widthAttrib=glGetAttribLocation(pixelShader,"width");
-
-  glVertexAttribPointer(posAttrib,3,GL_FLOAT,GL_FALSE,bytestride,(void*)(0));
-  glEnableVertexAttribArray(posAttrib);
-  
-  glVertexAttribIPointer(materialAttrib,1,GL_INT,bytestride,(void *) (3*size));
-  glEnableVertexAttribArray(materialAttrib);
-  
-  glVertexAttribPointer(widthAttrib,1,GL_FLOAT,GL_FALSE,bytestride,(void *) (4*size));
-  glEnableVertexAttribArray(widthAttrib);
-  
-  glDrawArrays(GL_POINTS,0,vertexbuffer.size());
-
-  glDisableVertexAttribArray(posAttrib);
-  glDisableVertexAttribArray(materialAttrib);
-  glDisableVertexAttribArray(widthAttrib);
-  
-  deleteUniforms();
-  
-  glBindBuffer(GL_ARRAY_BUFFER,0);
-
-  glBindVertexArray(0);
-  glDeleteVertexArrays(1,&vao);
 }
 
 #endif

commit 1af6f8a4adda6fd175b72265fe1a3ddd6d0e95db

Fix transparency bug introduced in a05450337791d59966d12fedecb19e73bebc2415.

bezierpatch.cc
@@ -820,9 +820,12 @@ void transform(const std::vector<VertexData>& b)
 //  ybuffer.resize(n);
   zbuffer.resize(n);
   
+  double Tz0=gl::dView[2];
+  double Tz1=gl::dView[6];
+  double Tz2=gl::dView[10];
   for(unsigned i=0; i < n; ++i) {
     const GLfloat *v=b[i].position;
-    zbuffer[i]=TransformZ(triple(v[0],v[1],v[2]),gl::dprojView);
+    zbuffer[i]=Tz0*v[0]+Tz1*v[1]+Tz2*v[2];
   }
 }
 

glrender.cc
@@ -197,6 +197,7 @@ dmat4 dviewMat;
 dmat4 drotateMat; 
 
 const double *dprojView;
+const double *dView;
 double BBT[9];
 
 GLuint ubo;
@@ -323,6 +324,7 @@ void home(bool webgl=false)
 #endif
 #endif
   dviewMat=dmat4(1.0);
+  dView=value_ptr(dviewMat);
   viewMat=mat4(dviewMat);
   
   drotateMat=dmat4(1.0); 
@@ -877,6 +879,7 @@ void update()
   
   dviewMat=translate(translate(dmat4(1.0),dvec3(cx,cy,cz))*drotateMat,
                      dvec3(0,0,-cz));
+  dView=value_ptr(dviewMat);
   viewMat=mat4(dviewMat);
 
   setProjection();

glrender.h
@@ -147,6 +147,7 @@ void glrender(const string& prefix, const camp::picture* pic,
               double *diffuse, double *specular, bool view, int oldpid=0);
 
 extern const double *dprojView;
+extern const double *dView;
 
 extern double BBT[9];
 