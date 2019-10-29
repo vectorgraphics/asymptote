b4dab27669302117b1b7c04857b08eb8b13c2406
John Bowman <bowman@ualberta.ca>
2019-10-29 09:02:39 -0600

    Fix xasy handling of Ctrl-c.

--- GUI/xasy.py
+++ GUI/xasy.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-import sys
+import sys,signal
 import PyQt5.QtWidgets as Qw
 from Window1 import MainWindow1
 
 
 def main(args):
     qtApp = Qw.QApplication(args)
+    signal.signal(signal.SIGINT,signal.SIG_DFL)
     mainWin1 = MainWindow1()
     mainWin1.show()
     return qtApp.exec_()
