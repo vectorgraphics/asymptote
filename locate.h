d541f69d20ed7d7a88e25cfe08323780796439ff
John Bowman <bowman@ualberta.ca>
2020-11-19 14:30:13 -0700

    Qualify namespace.

--- locate.h
+++ locate.h
@@ -18,7 +18,7 @@ extern file_list_t searchPath;
 
 // Find the appropriate file, first looking in the local directory, then the
 // directory given in settings, and finally the global system directory.
-string locateFile(string id, bool full=false, string suffix=suffix);
+string locateFile(string id, bool full=false, string suffix=settings::suffix);
 
 namespace fs {
 
