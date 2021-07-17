86c8c7924bbf1c85d64f2b0939c2f70e5d614044
John Bowman <bowman@ualberta.ca>
2021-07-17 12:43:59 -0600

    Fix last revision.

--- psfile.cc
+++ psfile.cc
@@ -211,7 +211,7 @@ void psfile::setopacity(const pen& p)
 
   string outputformat=settings::getSetting<string>("outformat");
   if(p.opacity() != lastpen.opacity() &&
-     (pdf || outputformat == "pdf" || outputformat == "html" ||
+     ((pdftex() && outputformat == "") || outputformat == "pdf" || outputformat == "html" ||
       outputformat == "svg" || outputformat == "png")) {
     *out << p.opacity() << " .setfillconstantalpha" << newl
          << p.opacity() << " .setstrokeconstantalpha" << newl;
