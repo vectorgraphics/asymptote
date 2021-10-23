c567e4b482d2f58eccde51aa510e61a549d49778
John Bowman <bowman@ualberta.ca>
2021-10-23 01:33:43 -0600

    Simplify code.

--- base/shaders/partialsum.glsl
+++ base/shaders/partialsum.glsl
@@ -14,22 +14,6 @@ layout(binding=1) buffer Data
 
 shared uint sharedData[gl_WorkGroupSize.x];
 
-// Return ceil(log2(n))
-uint ceillog2(uint n)
-{
-  const int MultiplyDeBruijnBitPosition[32]={
-  0,9,1,10,13,21,2,29,11,14,16,18,22,25,3,30,
-  8,12,20,28,15,17,24,7,19,27,23,6,26,5,4,31
-  };
-  n |= n >> 1;
-  n |= n >> 2;
-  n |= n >> 4;
-  n |= n >> 8;
-  n |= n >> 16;
-
-  return MultiplyDeBruijnBitPosition[n*0x07C4ACDDU >> 27];
-}
-
 uint ceilquotient(uint a, uint b)
 {
   return (a+b-1)/b;
@@ -42,7 +26,7 @@ void main(void)
 
   barrier();
 
-  const uint steps=ceillog2(gl_WorkGroupSize.x); // Precompute this?
+  const uint steps=10; // ceil(log2(gl_WorkGroupSize.x));
 
   for(uint step=0; step < steps; step++) {
     uint mask=1 << step;
