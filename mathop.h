vectorgraphics/asymptote

commit 1e1e6cbbcfe65b58ead8e04d5e2b302ed011acd8

Fix integer division bug (due to overflow).

mathop.h
@@ -238,21 +238,23 @@ struct power<Int> {
   }
 };
  
-template <typename T>
+template <typename>
 struct mod {
-  T operator() (T x, T y,  size_t i=0) {
+  Int operator() (Int x, Int y,  size_t i=0) {
     if(y == 0) dividebyzero(i);
     return portableMod(x,y);
   }
 };
 
-template <typename T>
+template <typename>
 struct quotient {
-  T operator() (T x, T y,  size_t i=0) {
+  Int operator() (Int x, Int y,  size_t i=0) {
     if(y == 0) dividebyzero(i);
     if(y == -1) return Negate(x);
 // Implementation-independent definition of integer division: round down
-    return (x-portableMod(x,y))/y;
+    Int q=x/y;
+    if(q >= 0 || y*q == x) return q;
+    return q-1;
   }
 };
 

commit 1d0e4cc886b840f87a726b40e6f9626b14568acc

Fix integer quotient bug introduced in 1e1e6cbbcfe65b58ead8e04d5e2b302ed011acd8.

mathop.h
@@ -249,7 +249,7 @@ struct quotient {
     if(y == -1) return Negate(x);
 // Implementation-independent definition of integer division: round down
     Int q=x/y;
-    if(q >= 0 || y*q == x) return q;
+    if(!((x < 0)^(y < 0)) || y*q == x) return q;
     return q-1;
   }
 };
@@ -325,4 +325,3 @@ void interp(vm::stack *s)
 } // namespace run
 
 #endif //MATHOP_H
-

tests/arith/integer.asy
@@ -1,23 +1,32 @@
 // Integer arithmetic.
 import TestLib;
 StartTest("integer addition");
-assert(1+1==2);
+assert(1+1 == 2);
 EndTest();
 StartTest("integer subtraction");
-assert(2-1==1);
+assert(2-1 == 1);
 EndTest();
 StartTest("integer multiplication");
-assert(2*2==4);
+assert(2*2 == 4);
 EndTest();
 StartTest("integer division");
-assert(4/2==2);
+assert(4/2 == 2);
+assert(3/2 == 1.5);
+EndTest();
+StartTest("integer quotient");
+assert(4#2 == 2);
+assert(3#2 == 1);
+assert(1#2 == 0);
+assert(-1#2 == -1);
+assert(1#-2 == -1);
+assert(-1#-2 == 0);
+assert(-3#2 == -2);
 EndTest();
 StartTest("integer self ops");
-{ int x = 3; assert(++x==4); assert(x==4); }
-{ int x = 3; assert(--x==2); assert(x==2); }
-{ int x = 3; assert((x+=7) == 10); assert(x==10); }
-{ int x = 3; assert((x-=7) == -4); assert(x==-4); }
-{ int x = 3; assert((x*=7) == 21); assert(x==21); }
-{ int x = 10; assert((x%=4) == 2); assert(x==2); }
+{ int x=3; assert(++x == 4); assert(x == 4); }
+{ int x=3; assert(--x == 2); assert(x == 2); }
+{ int x=3; assert((x += 7)  ==  10); assert(x == 10); }
+{ int x=3; assert((x -= 7)  ==  -4); assert(x == -4); }
+{ int x=3; assert((x *= 7)  ==  21); assert(x == 21); }
+{ int x=10; assert((x %= 4)  ==  2); assert(x == 2); }
 EndTest();
-