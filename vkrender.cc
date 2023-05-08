f05921b7f773edd2a4dfde8e59dd3d3de0d4cc37
Cole White <c-m-w-other@tuta.io>
2023-05-08 15:51:29 -0600

    Display filename when failed to open file.

--- vkrender.cc
+++ vkrender.cc
@@ -84,7 +84,7 @@ std::vector<char> readFile(const std::string& filename)
 {
   std::ifstream file(filename, std::ios::ate | std::ios::binary);
   if (!file.is_open()) {
-    throw std::runtime_error("failed to open file!");
+    throw std::runtime_error("failed to open file " + filename + "!");
   }
 
   std::size_t fileSize = (std::size_t) file.tellg();
