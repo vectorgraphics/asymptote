3d51a153fa02a43ae705b655de8f514a8db9db83
Cole White <c-m-w-other@tuta.io>
2023-07-20 12:10:53 -0600

    Remove old shaders.

--- shaders/blendIndexing.vert.glsl
+++
@@ -1,8 +0,0 @@
-#version 450
-#define GPUINDEXING
-
-void main()
-{
-  vec2 vertices[3]=vec2[3](vec2(-1,-1),vec2(3,-1),vec2(-1, 3));
-  gl_Position = vec4(vertices[gl_VertexIndex],0,1);
-}
--- shaders/blendIndexingSSBO.vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 450
-#define GPUINDEXING
-#define HAVE_SSBO
-
-void main()
-{
-  vec2 vertices[3]=vec2[3](vec2(-1,-1),vec2(3,-1),vec2(-1, 3));
-  gl_Position = vec4(vertices[gl_VertexIndex],0,1);
-}
