c602f03f4fff1d16e4a402aaef041af9cdb7ddd7
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-10-22 02:42:08 -0600

    CMAKE: Add HAVE_UNORDERED_MAP to compilation macro.

--- cmake-scripts/asy-macro.cmake
+++ cmake-scripts/asy-macro.cmake
@@ -3,3 +3,8 @@ list(APPEND ASY_MACROS
         PACKAGE_VERSION="${ASY_VERSION}"
         PACKAGE_BUGREPORT="${ASY_BUGREPORT}"
 )
+
+# Since we require C++11 and up, some macros are automatically included
+list(APPEND ASY_MACROS
+        HAVE_UNORDERED_MAP
+)
