22c709375c373072fb8e81762f057daa8946617e
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-10-22 02:41:13 -0600

    ZLIB: Add zlib to cmake lists.

--- cmake-scripts/external-libs.cmake
+++ cmake-scripts/external-libs.cmake
@@ -7,3 +7,10 @@ else()
     # only include lsp libraries
     list(APPEND ASYMPTOTE_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/LspCpp/include)
 endif()
+
+find_package(tinyexr CONFIG REQUIRED)
+list(APPEND ASY_STATIC_LIBARIES unofficial::tinyexr::tinyexr)
+
+# zlib
+find_package(ZLIB REQUIRED)
+list(APPEND ASY_STATIC_LIBARIES ZLIB::ZLIB)
