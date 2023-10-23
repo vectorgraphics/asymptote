2e696b8e67b4ef6b923c97057feabad3df05ffcc
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-10-23 02:00:34 -0600

    CMAKE: Copy base files to build directory.

---
+++ cmake-scripts/asy-base-files.cmake
@@ -0,0 +1,48 @@
+set(ASY_STATIC_BASE_FILES
+        animate animation annotate babel bezulate binarytree bsp CAD colormap
+        contour3 contour drawtree embed external feynman flowchart fontsize
+        geometry graph3 graph graph_settings graph_splinetype grid3 interpolate
+        labelpath3 labelpath lmfit map markers math metapost obj ode palette patterns
+        plain_arcs plain_arrows plain plain_bounds plain_boxes plain_constants plain_debugger
+        plain_filldraw plain_Label plain_margins plain_markers plain_paths plain_pens
+        plain_picture plain_prethree plain_scaling plain_shipout plain_strings pstoedit rational rationalSimplex
+        roundedpath simplex size10 size11 slide slopefield smoothcontour3 solids stats syzygy
+        texcolors three_arrows three three_light three_margins three_surface three_tube tree
+        trembling tube v3d x11colors
+)
+
+set(ASY_STATIC_SHADER_FILES
+        blend compress count fragment screen sum1 sum2 sum2fast
+        sum3 vertex zero
+)
+
+# base dir
+set(ASY_SOURCE_BASE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/base)
+set(ASY_BUILD_BASE_DIR ${CMAKE_CURRENT_BINARY_DIR}/base)
+
+file(MAKE_DIRECTORY ${ASY_BUILD_BASE_DIR})
+
+# version.asy
+configure_file(${ASY_RESOURCE_DIR}/versionTemplate.asy.in ${ASY_BUILD_BASE_DIR}/version.asy)
+
+# copy base files to build dir
+
+macro (copy_base_file base_file_name)
+    add_custom_command(
+            TARGET asy
+            POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy
+            ${ASY_SOURCE_BASE_DIR}/${base_file_name}
+            ${ASY_BUILD_BASE_DIR}/${base_file_name}
+            BYPRODUCTS ${ASY_BUILD_BASE_DIR}/${base_file_name}
+    )
+endmacro()
+
+foreach(ASY_STATIC_BASE_FILE ${ASY_STATIC_BASE_FILES})
+    copy_base_file(${ASY_STATIC_BASE_FILE}.asy)
+endforeach ()
+
+file(MAKE_DIRECTORY ${ASY_BUILD_BASE_DIR}/shaders)
+foreach(ASY_STATIC_SHADER_FILE ${ASY_STATIC_SHADER_FILES})
+    copy_base_file(shaders/${ASY_STATIC_SHADER_FILE}.glsl)
+endforeach ()
