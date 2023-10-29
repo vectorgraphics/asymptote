94955792af93477fb0d5a700ff395b53c1e98b31
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-10-29 00:53:53 -0600

    CMAKE: Use backports/getopt for windows builds.

---
+++ cmake-scripts/win32-specific.cmake
@@ -0,0 +1,9 @@
+if (NOT WIN32)
+    message(FATAL_ERROR "This file is only for use with windows.")
+endif()
+
+# for getopt
+add_subdirectory(backports/getopt)
+
+list(APPEND ASY_STATIC_LIBARIES getopt)
+list(APPEND ASYMPTOTE_INCLUDES $<TARGET_PROPERTY:getopt,INCLUDE_DIRECTORIES>)
