cfa9a669968a22812b85529f9db9b924451450ec
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-11-03 14:35:23 -0600

    UTIL: Make util.cc win32 compatible.

--- src/util.cc
+++ src/util.cc
@@ -15,7 +15,6 @@
 #include <cfloat>
 #include <sstream>
 #include <cerrno>
-#include <sys/wait.h>
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -24,6 +23,13 @@
 #include <cstring>
 #include <algorithm>
 #include <dirent.h>
+
+#if !defined(_WIN32)
+#include <sys/wait.h>
+#else
+#include <Windows.h>
+#include "win32helpers.h"
+#endif
 
 #include "util.h"
 #include "settings.h"
@@ -34,6 +40,7 @@
 
 using namespace settings;
 using camp::reportError;
+namespace w32 = camp::w32;
 
 bool False=false;
 
@@ -64,6 +71,115 @@ string demangle(const char* s)
 {
   return s;
 }
+#endif
+
+#pragma
+// windows specific unnamed spaces
+#if defined(_WIN32)
+namespace
+{
+/** @brief System, but for Windows.
+ *         Any handle placed in outHandle must be properly closed */
+int SystemWin32(const mem::vector<string>& command, int quiet, bool wait,
+                const char* hint, const char* application, int* ppid);
+
+string buildWindowsCmd(const mem::vector<string>& command);
+
+}
+
+namespace
+{
+string buildWindowsCmd(const mem::vector<string>& command)
+{
+  ostringstream out;
+  for (auto it= command.begin(); it != command.end(); ++it)
+  {
+    out << '"' << *it << '"';
+    if (std::next(it) != command.end())
+    {
+      out << ' ';
+    }
+  }
+
+  return out.str();
+}
+
+int SystemWin32(const mem::vector<string>& command, int quiet, bool wait,
+                const char* hint, const char* application, int* ppid)
+{
+  cout.flush();
+  if (command.empty())
+  {
+    camp::reportError("Command cannot be empty");
+    return -1;
+  }
+
+  string cmdlineStr=buildWindowsCmd(command);
+
+  STARTUPINFOA startInfo={};
+  startInfo.cb=sizeof(startInfo);
+  startInfo.dwFlags=STARTF_USESTDHANDLES;
+
+  // windows' "/dev/null" file (a.k.a. "NUL")
+  HANDLE nulFile=CreateFileA(
+          "NUL", GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ,
+          nullptr,
+          OPEN_EXISTING,
+          FILE_ATTRIBUTE_NORMAL,
+          nullptr);
+
+  // set quiet info
+  startInfo.hStdInput= GetStdHandle(STD_INPUT_HANDLE);
+  startInfo.hStdOutput= quiet >= 1 ? nulFile : GetStdHandle(STD_OUTPUT_HANDLE);
+  startInfo.hStdOutput= quiet >= 2 ? nulFile : GetStdHandle(STD_ERROR_HANDLE);
+
+  PROCESS_INFORMATION procInfo={};
+
+  w32::checkResult(CreateProcessA(
+                           command.at(0).c_str(),
+                           cmdlineStr.data(),
+                           nullptr, nullptr, false,
+                           0,
+                           nullptr, nullptr,
+                           &startInfo,
+                           &procInfo),
+                   "Cannot open process");
+
+  w32::checkResult(CloseHandle(nulFile), "Cannot close null file handle");
+
+  if (ppid)
+  {
+    *ppid=procInfo.dwProcessId;
+  }
+
+  w32::HandleRaiiWrapper procHandle(procInfo.hProcess);
+  w32::HandleRaiiWrapper threadHandle(procInfo.hThread);
+
+
+  if (!wait)
+  {
+    return 0;
+  }
+
+  DWORD retcode=-1;
+  // else, wait
+  switch (WaitForSingleObject(procHandle.getHandle(), INFINITE))
+  {
+    case WAIT_OBJECT_0: {
+      retcode=GetExitCodeProcess(procHandle.getHandle(), &retcode);
+      break;
+    }
+    case WAIT_ABANDONED:// also impossible, we are waiting for a process
+    case WAIT_TIMEOUT:  // impossible, since we set timeout to infinite
+    case WAIT_FAILED:
+    default:
+      camp::reportError("Waiting for process failed");
+      break;
+  }
+  return static_cast<int>(retcode);
+}
+}
+
 #endif
 
 char *Strdup(string s)
@@ -209,12 +325,16 @@ string auxname(string filename, string suffix)
 
 sighandler_t Signal(int signum, sighandler_t handler)
 {
+#if !defined(_WIN32)
   struct sigaction action,oldaction;
   action.sa_handler=handler;
   sigemptyset(&action.sa_mask);
   action.sa_flags=0;
   return sigaction(signum,&action,&oldaction) == 0 ? oldaction.sa_handler :
     SIG_ERR;
+#else
+  return signal(signum, handler);
+#endif
 }
 
 void push_split(mem::vector<string>& a, const string& S)
@@ -278,6 +398,9 @@ void execError(const char *command, const char *hint, const char *application)
 int System(const mem::vector<string> &command, int quiet, bool wait,
            const char *hint, const char *application, int *ppid)
 {
+#if _WIN32
+    return SystemWin32(command, quiet, wait, hint, application, ppid);
+#else
   int status;
 
   cout.flush(); // Flush stdout to avoid duplicate output.
@@ -287,10 +410,6 @@ int System(const mem::vector<string> &command, int quiet, bool wait,
   int pid=fork();
   if(pid == -1)
     camp::reportError("Cannot fork process");
-
-#ifdef __MSDOS__
-  wait=true;
-#endif
 
   if(pid == 0) {
     if(interact::interactive) signal(SIGINT,SIG_IGN);
@@ -343,6 +462,7 @@ int System(const mem::vector<string> &command, int quiet, bool wait,
       }
     }
   }
+#endif
 }
 
 string stripblanklines(const string& s)
@@ -415,6 +535,25 @@ void push_command(mem::vector<string>& a, const string& s)
 }
 
 void popupHelp() {
+#if defined(_WIN32)
+  mem::vector<string> cmd;
+  push_command(cmd, getSetting<string>("pdfviewer"));
+  if (auto viewerOpts = getSetting<string>("pdfviewerOptions"); !viewerOpts.empty())
+  {
+    istringstream viewerOptStream(viewerOpts);
+    string tmp;
+    while (viewerOptStream >> tmp)
+    {
+      if (!tmp.empty())
+      {
+        cmd.push_back(tmp);
+      }
+    }
+  }
+
+  cmd.push_back(docdir+dirsep+"asymptote.pdf");
+  System(cmd,0,false,"pdfviewer","your PDF viewer");
+#else
   // If the popped-up help is already running, pid stores the pid of the viewer.
   static int pid=0;
 
@@ -432,6 +571,7 @@ void popupHelp() {
     cmd.push_back(docdir+dirsep+"asymptote.pdf");
     status=System(cmd,0,false,"pdfviewer","your PDF viewer",&pid);
   }
+#endif
 }
 
 const char *intrange="integer argument is outside valid range";
