703f0443f7e9261f9ffd984191a792a5f0d8c4cd
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-11-10 20:31:20 -0700

    CMAKE: Remove tinyexr from external-libs (vcpkg).

--- cmake-scripts/external-libs.cmake
+++ cmake-scripts/external-libs.cmake
@@ -9,9 +9,6 @@ else()
     list(APPEND ASYMPTOTE_INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}/LspCpp/include)
 endif()
 
-find_package(tinyexr CONFIG REQUIRED)
-list(APPEND ASY_STATIC_LIBARIES unofficial::tinyexr::tinyexr)
-
 # zlib
 find_package(ZLIB REQUIRED)
 list(APPEND ASY_STATIC_LIBARIES ZLIB::ZLIB)
