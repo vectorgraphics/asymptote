27c2d390036be5ea51e57c19b350e4d20c87debb
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-12-05 12:39:27 -0700

    MSVC: Attempt to automatically locate g++ or clang++ if GCCCOMPAT_CXX_COMPILER_FOR_MSVC is not given.

--- cmake-scripts/win32-specific.cmake
+++ cmake-scripts/win32-specific.cmake
@@ -19,19 +19,59 @@ list(APPEND ASY_MACROS ASYMPTOTE_SYSDIR="")
 
 set(BUILD_SHARED_LIBS OFF)
 
+# gcc/clang++ compatible validators
+
+function(validate_gcc_compat_cxx validator_result_var gcccompat_compiler)
+    execute_process(
+            COMMAND ${gcccompat_compiler} "--version"
+            OUTPUT_VARIABLE COMPILER_RESULT)
+
+    if (NOT COMPILER_RESULT MATCHES 
+        "(clang version )|(g\\+\\+\\.exe \\(MinGW\\))|(g\\+\\+ \\(GCC\\))")
+        set(${validator_result_var} FALSE PARENT_SCOPE)
+    endif()
+endfunction()
+
+
+macro(find_compatible_gcc_compilers)
+    find_program(
+        GCCCOMPAT_CXX_COMPILER_FOUND
+        NAMES clang++ g++
+        VALIDATOR validate_gcc_compat_cxx
+    )
+
+    if (GCCCOMPAT_CXX_COMPILER_FOUND)
+        message(STATUS "Found clang++/g++ at ${GCCCOMPAT_CXX_COMPILER_FOUND}")
+        set(GCCCOMPAT_CXX_COMPILER_FOR_MSVC ${GCCCOMPAT_CXX_COMPILER_FOUND})
+    endif()
+endmacro()
+
+# attempting to find gcc-compatible C++ compiler
+
 if (MSVC)
     if (NOT GCCCOMPAT_CXX_COMPILER_FOR_MSVC)
-        message(
-                WARNING
-                "\
+        message(STATUS "GCCCOMPAT_CXX_COMPILER_FOR_MSVC not given, will attempt to find g++.exe or clang++.exe")
+        find_compatible_gcc_compilers()
+    endif()
+
+    if (GCCCOMPAT_CXX_COMPILER_FOR_MSVC)
+        message(STATUS "Using GCC-compatible C++ compiler at \
+${GCCCOMPAT_CXX_COMPILER_FOR_MSVC} for preprocessing.")
+    endif()
+
+    if (NOT GCCCOMPAT_CXX_COMPILER_FOR_MSVC)
+        message(WARNING "\
 GCC-compatible C++ compiler not specified, target dependency resolution for generated files may \
 not work properly. If you are looking for a GCC-compatible C++ compiler on windows for preprocessing, \
 we recommend the LLVM toolchain. You can find LLVM at \
 \
-https://releases.llvm.org/download.html\
+https://releases.llvm.org/download.html \
 \
-or through msys2."
-        )
+or through msys2. After that, re-run cmake with either \
+-DGCCCOMPAT_CXX_COMPILER_FOR_MSVC=<path to clang++.exe> \
+or if running msvc/* presets, with GCCCOMPAT_CXX_COMPILER_FOR_MSVC \
+environment variable set to the path \
+to clang++.exe.")
     endif()
 endif()
 
