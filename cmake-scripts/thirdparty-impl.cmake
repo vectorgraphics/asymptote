9192809d1841a8093bff3f03fb07bb27cb20f429
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-01-09 02:05:01 -0700

    CMAKE: Add VMA to build process.

--- cmake-scripts/thirdparty-impl.cmake
+++ cmake-scripts/thirdparty-impl.cmake
@@ -1,6 +1,13 @@
 # tinyexr
 
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty_impl/tinyexr_impl/)
-list(APPEND ASY_STATIC_LIBARIES tinyexr-impl)
+set(THIRDPARTY_IMPL_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty_impl)
 
+add_subdirectory(${THIRDPARTY_IMPL_ROOT}/tinyexr_impl/)
+list(APPEND ASY_STATIC_LIBARIES tinyexr-impl)
 list(APPEND ASYMPTOTE_INCLUDES $<TARGET_PROPERTY:tinyexr-impl,INCLUDE_DIRECTORIES>)
+
+if (ENABLE_VULKAN)
+    add_subdirectory(${THIRDPARTY_IMPL_ROOT}/vk-mem-allocator_impl)
+    list(APPEND ASY_STATIC_LIBARIES vk-mem-allocator-impl)
+    list(APPEND ASYMPTOTE_INCLUDES $<TARGET_PROPERTY:vk-mem-allocator-impl,INCLUDE_DIRECTORIES>)
+endif()
