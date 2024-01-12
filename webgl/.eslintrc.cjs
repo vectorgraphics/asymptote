a2fa7699d45873124fbc1d6339146fac08b0636a
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-01-11 22:02:06 -0700

    NODE: Set up basic libraries.

---
+++ webgl/.eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
