db98e4b9182e662cbf3374e7c91fcc2a31cc965a
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-02-29 12:09:33 -0700

    CMAKE: Add support for custom version suffix.
    (cherry picked from commit d05ccd29522b043c74d8992058bf883375b64e07)

--- pkg-info.cmake
+++ pkg-info.cmake
@@ -1,4 +1,6 @@
 set(ASY_PACKAGE_NAME Asymptote)
-set(ASY_VERSION 2.87git-cmake)
+
+# ASY_VERSION is set by base version + suffix, if present
+set(ASY_VERSION_BASE 2.87git-cmake)
 set(ASY_BUGREPORT "https://github.com/vectorgraphics/asymptote/issues")
 set(ASY_GL_VERSION 1.02)
