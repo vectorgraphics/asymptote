fd497578538a3b2c589d0a6fea51823a58740a45
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-02-29 11:29:14 -0700

    CMAKE: Bump version.

--- pkg-info.cmake
+++ pkg-info.cmake
@@ -1,4 +1,4 @@
 set(ASY_PACKAGE_NAME Asymptote)
-set(ASY_VERSION 2.87git-cmake)
+set(ASY_VERSION 2.88)
 set(ASY_BUGREPORT "https://github.com/vectorgraphics/asymptote/issues")
 set(ASY_GL_VERSION 1.02)
