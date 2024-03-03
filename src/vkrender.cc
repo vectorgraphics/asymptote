23f934b33709c5984b0028001da3cd565fb10b07
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-03-02 20:37:00 -0700

    VK: Move fxaa & srgb options before return in modifyShaderOptions since we are also using that macro in fragment & blend shader.

--- src/vkrender.cc
+++ src/vkrender.cc
@@ -3046,11 +3046,22 @@ void AsyVkRender::modifyShaderOptions(std::vector<std::string>& options, Pipelin
     options.emplace_back("ORTHOGRAPHIC");
   }
 
+  if (fxaa)
+  {
+    options.emplace_back("ENABLE_FXAA");
+  }
+
+  if (srgb)
+  {
+    options.emplace_back("OUTPUT_AS_SRGB");
+  }
+
   if (type == PIPELINE_OPAQUE) {
     options.emplace_back("OPAQUE");
     return;
   }
 
+  // from now on, only things relevant to compute
   if (GPUcompress) {
     options.emplace_back("GPUCOMPRESS");
   }
@@ -3061,16 +3072,6 @@ void AsyVkRender::modifyShaderOptions(std::vector<std::string>& options, Pipelin
   options.emplace_back("LOCALSIZE " + std::to_string(localSize));
   options.emplace_back("BLOCKSIZE " + std::to_string(blockSize));
   options.emplace_back("ARRAYSIZE " + std::to_string(maxSize));
-
-  if (fxaa)
-  {
-    options.emplace_back("ENABLE_FXAA");
-  }
-
-  if (srgb)
-  {
-    options.emplace_back("OUTPUT_AS_SRGB");
-  }
 }
 
 template<typename V>
