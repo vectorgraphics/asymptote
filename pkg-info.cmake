b66715648df3559650101d343f5c34399e90bc8b
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-04-11 11:32:45 -0600

    ASY: Increment version to 2.90 for cmake.

--- pkg-info.cmake
+++ pkg-info.cmake
@@ -1,6 +1,6 @@
 set(ASY_PACKAGE_NAME Asymptote)
 
 # ASY_VERSION is set by base version + suffix, if present
-set(ASY_VERSION_BASE 2.88)
+set(ASY_VERSION_BASE 2.90)
 set(ASY_BUGREPORT "https://github.com/vectorgraphics/asymptote/issues")
 set(ASY_GL_VERSION 1.02)
