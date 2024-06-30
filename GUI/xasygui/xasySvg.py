a33ee4b660319b3335a36c86c6e09966ef99f075
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-06-30 16:53:04 -0600

    XASY: Add support for custom svg converter path.

--- GUI/xasygui/xasySvg.py
+++ GUI/xasygui/xasySvg.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python3
-
 import PyQt5.QtGui as QtGui
 import PyQt5.QtWidgets as QtWidgets
-import io
 import subprocess
 import sys
+from xasygui import xasyOptions as xo
 
-class SvgObject():
+
+class SvgObject:
     def __init__(self, file: str):
-        self.file=file
+        self.file = file
 
-    def render(self, dpi:int) -> QtGui.QImage:
+    def _create_call_arguments(self, dpi: int):
+        settings = xo.BasicConfigs.defaultOpt
+        return [
+            settings.get("rsvgConverterPath") or "rsvg-convert",
+            f"--dpi-x={dpi}",
+            f"--dpi-y={dpi}",
+            "--format=png",
+            self.file,
+        ]
+
+    def render(self, dpi: int) -> QtGui.QImage:
+        callArgs = self._create_call_arguments(dpi)
         try:
-            rawDataProc = subprocess.Popen(['rsvg-convert', '--dpi-x', str(dpi),
-                                            '--dpi-y', str(dpi), self.file],
-                                           stdout=subprocess.PIPE)
-        except:
-            QtWidgets.QMessageBox.about(None,'rsvg-convert missing','Please install rsvg-convert version >= 2.40 in your path.')
+            rawDataProc = subprocess.run(
+                callArgs,
+                stdout=subprocess.PIPE,
+            )
+        except OSError:
+            QtWidgets.QMessageBox.about(
+                None,
+                "rsvg-convert missing",
+                "Please install rsvg-convert version >= 2.40 is available.",
+            )
             sys.exit(-1)
 
-        return QtGui.QImage.fromData(rawDataProc.stdout.read(), 'PNG')
+        return QtGui.QImage.fromData(rawDataProc.stdout, "PNG")
