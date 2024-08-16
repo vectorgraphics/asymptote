aebd49b7eea14eeb04ec9aa644158ad81b52a862
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-08-15 21:24:21 -0600

    CMAKE: Generate remaining documentation-related asymptote files in docgen.

--- cmake-scripts/docgen.cmake
+++ cmake-scripts/docgen.cmake
@@ -7,7 +7,7 @@ set(ASY_TEX_BUILD_ROOT ${CMAKE_CURRENT_BINARY_DIR}/docbuild)
 file(MAKE_DIRECTORY ${ASY_TEX_BUILD_ROOT})
 configure_file(${ASY_RESOURCE_DIR}/version.texi.in ${ASY_TEX_BUILD_ROOT}/version.texi)
 
-set(LATEX_ARTIFRACT_EXTENSIONS aux hd idx ins log out)
+set(LATEX_ARTIFRACT_EXTENSIONS aux hd idx ins log out toc)
 
 find_package(LATEX COMPONENTS PDFLATEX REQUIRED)
 list(
@@ -57,12 +57,10 @@ set(ASY_DOC_FILE_PREFIXES
         triangulate unitcircle3 vectorfield
 )
 
-# handle CDlabel, filegraph, leastsquares, logo separately
-set(ASY_FILE_WITH_CSV diatom secondaryaxis westnile)
-
 set(ASY_DOC_PDF_FILES "")
 
 set(CMAKE_COPY_ASY_FILE_TO_DOCBUILD_BASE_ARGS ${CMAKE_COMMAND} -E copy -t ${ASY_TEX_BUILD_ROOT})
+set(CMAKE_RM_BASE_ARGUMENTS ${CMAKE_COMMAND} -E rm)
 set(ASY_BASE_ARGUMENTS asy -dir ${ASY_BUILD_BASE_DIR} -config '' -render=0 -noprc -noV)
 
 # independent asymptote files that can be generated with any other files
@@ -77,36 +75,92 @@ foreach(ASY_DOC_FILE_PREFIX ${ASY_DOC_FILE_PREFIXES})
             COMMAND ${CMAKE_COPY_ASY_FILE_TO_DOCBUILD_BASE_ARGS} ${ASY_DOC_ROOT}/${ASY_DOC_FILE_PREFIX}.asy
             COMMAND ${ASY_BASE_ARGUMENTS} -fpdf ${ASY_DOC_FILE_PREFIX}.asy
             # cleanup <buildroot>/file.asy
-            COMMAND ${CMAKE_COMMAND} -E rm ${ASY_TEX_BUILD_ROOT}/${ASY_DOC_FILE_PREFIX}.asy
+            COMMAND ${CMAKE_RM_BASE_ARGUMENTS}
+                ${ASY_TEX_BUILD_ROOT}/${ASY_DOC_FILE_PREFIX}.asy
             # cleanup tex artifacts, if exist
-            COMMAND ${CMAKE_COMMAND} -E rm -f ${ASY_TEX_BUILD_ROOT}/${ASY_DOC_FILE_PREFIX}_.tex
+            COMMAND ${CMAKE_RM_BASE_ARGUMENTS} -f
+                ${ASY_TEX_BUILD_ROOT}/${ASY_DOC_FILE_PREFIX}_.tex
             WORKING_DIRECTORY ${ASY_TEX_BUILD_ROOT}
     )
     list(APPEND ASY_DOC_PDF_FILES ${ASY_DOC_FILE_OUTPUT})
 endforeach()
 
-foreach(ASY_DOC_FILE_PREFIX ${ASY_DOC_FILE_PREFIXES})
-    set(ASY_DOC_FILE_OUTPUT ${ASY_TEX_BUILD_ROOT}/${ASY_DOC_FILE_PREFIX}.pdf)
+macro(add_asy_file_with_extension asy_file extra_ext)
+    set(ASY_DOC_FILE_OUTPUT ${ASY_TEX_BUILD_ROOT}/${asy_file}.pdf)
+    set(ASY_AUX_FILE_NAME ${asy_file}.${extra_ext})
     # asymptote has some problems (currently as writing this) with asy files involving tex
     # and output directory not matching, so a workaround is to copy to the doc build root
     add_custom_command(
             OUTPUT ${ASY_DOC_FILE_OUTPUT}
-            DEPENDS ${ASY_DOC_ROOT}/${ASY_DOC_FILE_PREFIX}.asy asy ${ASY_OUTPUT_BASE_FILES}
-            COMMAND ${CMAKE_COPY_ASY_FILE_TO_DOCBUILD_BASE_ARGS} ${ASY_DOC_ROOT}/${ASY_DOC_FILE_PREFIX}.asy
+            DEPENDS
+                ${ASY_DOC_ROOT}/${asy_file}.asy
+                ${ASY_DOC_ROOT}/${ASY_AUX_FILE_NAME}
+                asy ${ASY_OUTPUT_BASE_FILES}
+            COMMAND ${CMAKE_COPY_ASY_FILE_TO_DOCBUILD_BASE_ARGS}
+                ${ASY_DOC_ROOT}/${asy_file}.asy
+                ${ASY_DOC_ROOT}/${ASY_AUX_FILE_NAME}
             COMMAND ${ASY_BASE_ARGUMENTS} -fpdf ${ASY_DOC_FILE_PREFIX}.asy
-            COMMAND ${CMAKE_COMMAND} -E rm ${ASY_TEX_BUILD_ROOT}/${ASY_DOC_FILE_PREFIX}.asy
-            COMMAND ${CMAKE_COMMAND} -E rm -f ${ASY_TEX_BUILD_ROOT}/${ASY_DOC_FILE_PREFIX}_.tex
+            COMMAND ${CMAKE_RM_BASE_ARGUMENTS}
+                ${ASY_TEX_BUILD_ROOT}/${asy_file}.asy
+                ${ASY_TEX_BUILD_ROOT}/${ASY_AUX_FILE_NAME}
+            COMMAND ${CMAKE_RM_BASE_ARGUMENTS} -f
+                ${ASY_TEX_BUILD_ROOT}/${asy_file}_.tex
             WORKING_DIRECTORY ${ASY_TEX_BUILD_ROOT}
     )
     list(APPEND ASY_DOC_PDF_FILES ${ASY_DOC_FILE_OUTPUT})
+endmacro()
+
+# asy + csv Files
+foreach(ASY_DOC_FILE_PREFIX diatom secondaryaxis westnile)
+    add_asy_file_with_extension(${ASY_DOC_FILE_PREFIX} csv)
 endforeach()
 
+# asy + dat files
+foreach(ASY_DOC_FILE_PREFIX filegraph leastsquares)
+    add_asy_file_with_extension(${ASY_DOC_FILE_PREFIX} dat)
+endforeach()
+
+
+# handle CDlabel and logo separately
+
+# <doc-root>/logo.asy -> <build-root>/logo.asy
+add_custom_command(
+        OUTPUT ${ASY_TEX_BUILD_ROOT}/logo.asy
+        COMMAND ${CMAKE_COPY_ASY_FILE_TO_DOCBUILD_BASE_ARGS}
+            ${ASY_DOC_ROOT}/logo.asy
+        DEPENDS ${ASY_DOC_ROOT}/logo.asy
+)
+
+# <build-root>/logo.asy -> <build-root>/logo.pdf [+ artifacts]
+add_custom_command(
+        OUTPUT ${ASY_TEX_BUILD_ROOT}/logo.pdf
+        COMMAND ${ASY_BASE_ARGUMENTS} -fpdf logo.asy
+        DEPENDS ${ASY_TEX_BUILD_ROOT}/logo.asy
+        BYPRODUCTS ${ASY_TEX_BUILD_ROOT}/logo_.tex ${ASY_TEX_BUILD_ROOT}/logo_.eps
+        WORKING_DIRECTORY ${ASY_TEX_BUILD_ROOT}
+)
+
+# <build-root>/logo.asy + <doc-root>/CDLabel.asy -> <build-root>/CDLabel.pdf
+add_custom_command(
+        OUTPUT ${ASY_TEX_BUILD_ROOT}/CDLabel.pdf
+        DEPENDS ${ASY_DOC_ROOT}/CDLabel.asy asy ${ASY_OUTPUT_BASE_FILES} ${ASY_TEX_BUILD_ROOT}/logo.asy
+        # copy <docroot>/file.asy -> <buildroot>/file.asy
+        COMMAND ${CMAKE_COPY_ASY_FILE_TO_DOCBUILD_BASE_ARGS} ${ASY_DOC_ROOT}/CDLabel.asy
+        COMMAND ${ASY_BASE_ARGUMENTS} -fpdf CDLabel.asy
+        # cleanup <buildroot>/file.asy
+        COMMAND ${CMAKE_RM_BASE_ARGUMENTS} ${ASY_TEX_BUILD_ROOT}/CDLabel.asy
+        # cleanup tex artifacts, if exist
+        COMMAND ${CMAKE_RM_BASE_ARGUMENTS} -f ${ASY_TEX_BUILD_ROOT}/CDLabel_.tex
+        WORKING_DIRECTORY ${ASY_TEX_BUILD_ROOT}
+)
+
+list(APPEND ASY_DOC_PDF_FILES ${ASY_TEX_BUILD_ROOT}/logo.pdf ${ASY_TEX_BUILD_ROOT}/CDLabel.pdf )
 
 # options file
 add_custom_command(
         OUTPUT ${ASY_TEX_BUILD_ROOT}/options
         DEPENDS asy ${ASY_DOC_ROOT}/gen-asy-options-file.py
         COMMAND ${PY3_INTERPRETER} ${ASY_DOC_ROOT}/gen-asy-options-file.py
-            --asy-executable=$<TARGET_FILE:asy>
-            --output-file=${ASY_TEX_BUILD_ROOT}/options
+        --asy-executable=$<TARGET_FILE:asy>
+        --output-file=${ASY_TEX_BUILD_ROOT}/options
 )
