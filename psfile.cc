e2a1cb9b08a8fe634eb91691b8719118eb35c238
CC <ccworld1000@gmail.com>
2024-10-25 09:12:24 +0800

    Two identical versions
    choose one of them

--- psfile.cc
+++ psfile.cc
@@ -152,8 +152,7 @@ void psfile::prologue(const bbox& box)
 {
   header(true);
   BoundingBox(box);
-  *out << "%%Creator: " << settings::PROGRAM << " " << settings::VERSION
-       << REVISION <<  newl;
+  *out << "%%Creator: " << settings::PROGRAM << " " << settings::VERSION <<  newl;
 
   time_t t; time(&t);
   struct tm *tt = localtime(&t);
