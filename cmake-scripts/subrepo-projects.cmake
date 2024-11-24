99f67caa4fd8847b46afa5b4a0b56180937e15ed
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-11-24 14:37:18 -0700

    CMAKE: Use subrepo clones of lspcpp and tinyexr.

---
+++ cmake-scripts/subrepo-projects.cmake
@@ -0,0 +1,23 @@
+set(LSP_REPO_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/LspCpp)
+
+if (ENABLE_LSP)
+    message(STATUS "LSP Enabled.")
+    # disable New Boost version warning
+    set(Boost_NO_WARN_NEW_VERSIONS 1)
+    set(USE_SYSTEM_RAPIDJSON ON CACHE INTERNAL "Use system rapidjson")
+    set(LSPCPP_USE_CPP17 ON CACHE INTERNAL "C++17 mode")
+    set(LSPCPP_SUPPORT_BOEHM_GC ON CACHE INTERNAL "Use boehm GC")
+    # For transitive URI dependency
+    set(Uri_BUILD_DOCS OFF CACHE INTERNAL "build docs for uri")
+    set(Uri_BUILD_TESTS OFF CACHE INTERNAL "build tests for uri")
+
+    add_subdirectory(${LSP_REPO_ROOT})
+    list(APPEND ASY_STATIC_LIBARIES lspcpp)
+    list(APPEND ASY_MACROS HAVE_LSP=1)
+else()
+    # only include lsp libraries
+    message(STATUS "LSP Disabled. Will not have language server protocol support.")
+    list(APPEND ASYMPTOTE_INCLUDES ${LSP_REPO_ROOT}/include)
+endif()
+
+set(TINYEXR_SUBREPO_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/../../tinyexr)
--- cmake-scripts/thirdparty-impl.cmake
+++ cmake-scripts/thirdparty-impl.cmake
@@ -1,5 +1,6 @@
 # tinyexr
 
+set(TINYEXR_REPO_LOCATION ${CMAKE_CURRENT_SOURCE_DIR}/tinyexr CACHE INTERNAL "tinyexr repo location")
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty_impl/tinyexr_impl/)
 list(APPEND ASY_STATIC_LIBARIES tinyexr-impl)
 
