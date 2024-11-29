53ac190b36c6787a162f124279a804f2082d0c57
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-11-28 19:49:48 -0700

    CMAKE: Use subrepo directories for cmake builds.

--- cmake-scripts/external-libs.cmake
+++ cmake-scripts/external-libs.cmake
@@ -1,33 +1,6 @@
 include(FindPkgConfig)
 include(FetchContent)
 
-FetchContent_Declare(
-        lspcpp
-        GIT_REPOSITORY https://github.com/vectorgraphics/LspCpp
-        GIT_TAG master
-)
-
-if (ENABLE_LSP)
-
-    message(STATUS "LSP Enabled.")
-    # disable New Boost version warning
-    set(Boost_NO_WARN_NEW_VERSIONS 1)
-    set(USE_SYSTEM_RAPIDJSON ON CACHE INTERNAL "Use system rapidjson")
-    set(LSPCPP_USE_CPP17 ON CACHE INTERNAL "C++17 mode")
-    set(LSPCPP_SUPPORT_BOEHM_GC ON CACHE INTERNAL "Use boehm GC")
-    # For transitive URI dependency
-    set(Uri_BUILD_DOCS OFF CACHE INTERNAL "build docs for uri")
-    set(Uri_BUILD_TESTS OFF CACHE INTERNAL "build tests for uri")
-    FetchContent_MakeAvailable(lspcpp)
-    list(APPEND ASY_STATIC_LIBARIES lspcpp)
-    list(APPEND ASY_MACROS HAVE_LSP=1)
-else()
-    FetchContent_Populate(lspcpp)
-    # only include lsp libraries
-    message(STATUS "LSP Disabled. Will not have language server protocol support.")
-    list(APPEND ASYMPTOTE_INCLUDES ${lspcpp_SOURCE_DIR}/include)
-endif()
-
 # zlib
 find_package(ZLIB REQUIRED)
 list(APPEND ASY_STATIC_LIBARIES ZLIB::ZLIB)
--- cmake-scripts/thirdparty-impl.cmake
+++ cmake-scripts/thirdparty-impl.cmake
@@ -1,6 +1,7 @@
 # tinyexr
 
+set(TINYEXR_REPO_LOCATION ${CMAKE_CURRENT_SOURCE_DIR}/tinyexr CACHE INTERNAL "tinyexr repo location")
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/thirdparty_impl/tinyexr_impl/)
-list(APPEND ASY_STATIC_LIBARIES tinyexr-impl)
 
+list(APPEND ASY_STATIC_LIBARIES tinyexr-impl)
 list(APPEND ASYMPTOTE_INCLUDES $<TARGET_PROPERTY:tinyexr-impl,INCLUDE_DIRECTORIES>)
