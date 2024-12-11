607b655e99a4c7e18a90fc175108cc3b7ea55890
John Bowman <bowman@ualberta.ca>
2024-12-11 11:37:00 -0800

    Optimize integer mod.

--- mod.h
+++ mod.h
@@ -20,18 +20,10 @@ inline double Mod(double x, double y) {return fmod(x,y);}
 template<typename T>
 inline T portableMod(T x,T y)
 {
-// Implementation-independent definition of mod; ensure that result has
+// Implementation-independent definition of mod; ensure result has
 // same sign as divisor
   T val=Mod(x,y);
   return ((y > 0 && val >= 0) || (y < 0 && val <= 0)) ? val : val+y;
-}
-
-inline Int imod(Int x, Int y)
-{
-  if(y > 0 && (y & (y-1)) == 0)
-    return x & (y-1); // Use mask if y is a power of two
-  else
-    return portableMod<Int>(x,y);
 }
 
 inline Int imod(Int x, size_t y) {
@@ -39,6 +31,14 @@ inline Int imod(Int x, size_t y) {
     return x & (y-1); // Use mask if y is a power of two
   x %= (Int) y;
   return (x >= 0) ? x : x+(Int) y;
+}
+
+inline Int imod(Int x, Int y)
+{
+  if(y > 0)
+    return imod(x,(size_t) y);
+  else
+    return portableMod<Int>(x,y);
 }
 
 #endif
