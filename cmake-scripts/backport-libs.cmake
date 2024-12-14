9e227f201363173ce0e3f7f1056ed1cd7a708274
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-12-14 14:54:30 -0700

    CMAKE: Remove glew from backport includes.

--- cmake-scripts/backport-libs.cmake
+++ cmake-scripts/backport-libs.cmake
@@ -1,11 +1,3 @@
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/backports/optional)
 
 list(APPEND ASY_STATIC_LIBARIES OptionalBackport)
-
-if (ENABLE_OPENGL)
-    add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/backports/glew)
-    # using glew within the repo
-    list(APPEND ASY_STATIC_LIBARIES GLEW)
-
-    list(APPEND ASYMPTOTE_INCLUDES $<TARGET_PROPERTY:GLEW,INCLUDE_DIRECTORIES>)
-endif()
