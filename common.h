2bc3e822b240615787b1447fd65fbf8a464e1e98
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2024-12-15 04:09:41 -0700

    CXX: Define HAVE_VULKAN only if it's not already defined.

--- common.h
+++ common.h
@@ -40,8 +40,11 @@ using nonstd::make_optional;
 using std::make_pair;
 
 #if !defined(FOR_SHARED) && defined(HAVE_LIBVULKAN) && defined(HAVE_LIBGLM)
+
+#if !defined(HAVE_VULKAN)
  #define HAVE_VULKAN
- #endif
+#endif
+#endif
 
 #if defined(HAVE_LIBREADLINE) || defined(HAVE_LIBEDIT)
 #define HAVE_READLINE
