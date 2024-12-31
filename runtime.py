26c91e91025dfc68e5c823017c1dd8b8f0f6e5ec
Charles Staats III <charles.staats.iii@gmail.com>
2024-12-30 18:49:19 -0800

    Improve runtime.py.

--- runtime.py
+++ runtime.py
@@ -4,16 +4,107 @@ import os
 import re
 import sys
 
+STACK = "Stack"
+
+
+class Er:
+    errors = 0
+
+    @classmethod
+    def report_error(cls, filename, line, error):
+        sys.stderr.write(f"{filename}:{line}: {error}\n")
+        cls.errors = 1
+
+    @classmethod
+    def assoc_error(cls, filename, line, t):
+        cls.report_error(filename, line, f"no asy type associated to '{t}'")
+
+
+# Convert parameters into stack pop code in correct order
+def c_params(params_list):
+    global STACK
+    result = []
+    # Reverse order for correct push/pop
+    for p in reversed(params_list):
+        match = re.search(
+            r"^\s*"  # optional whitespace
+            r"(explicit)*"  # optional explicit
+            r"\s*"
+            r"(\w*(?:\s*\*)?)"  # type
+            r"\s*"
+            r"(\w*)"  # parameter name
+            r"(=*)([\w.+\-]*)",  # optional default value
+            p,
+        )
+        if not match:
+            continue
+        _, type_str, name, eqsign, val = match.groups()
+        template = "" if type_str == "item" else f"<{type_str}>"
+        def_val = f",{val}" if eqsign else ""
+        r = f"  {type_str} {name}=vm::pop{template}({STACK}{def_val});\n"
+        result.append(r)
+    return result
+
+
+def clean_type(v):
+    return re.sub(r"\s+", "", v)
+
+
+def clean_params(v: str) -> str:
+    return v.replace("\n", "")
+
+
+def symbolize(name, opsymbols):
+    # If name is alphanumeric/underscore, just SYM(...) it
+    if re.search(r"^[A-Za-z0-9_]+$", name):
+        return f"SYM({name})"
+    # If the name is recognized in opsymbols, substitute
+    if name in opsymbols:
+        return opsymbols[name]
+    # Check if it is "operator smth" form
+    opm = re.search(r"operator\s+(\w+)", name)
+    if opm and opm.group(1) in opsymbols:
+        return opsymbols[opm.group(1)]
+    # Otherwise, generate a trans(...) symbol
+    return f'symbol::trans("{name}")'
+
+
+def asy_params(params_string, filename, line, type_map, opsymbols):
+    # Split parameters by comma
+    params_list = re.split(r",\s*", params_string) if params_string else []
+    result = []
+    for p in params_list:
+        # (explicit)*\s*(\w*(?:\s*\*)?) => optional 'explicit', type, pointer
+        # (\w*) => parameter name, possibly something like "x"
+        # (=*) => optional equals sign for default parameter
+        match = re.search(
+            r"^\s*(explicit)*\s*"  # optional explicit
+            r"(\w*(?:\s*\*)?)\s*"  # type or pointer type
+            r"(\w*)"  # name
+            r"(=*)",  # if '=' is present, there is a default value
+            p,
+        )
+        if not match:
+            continue
+        explicit, t, n, d = match.groups()
+        t = clean_type(t)
+        if t not in type_map:
+            Er.assoc_error(filename, line, t)
+        has_def = "true" if d else "false"
+        is_ex = "true" if explicit else "false"
+        result.append(
+            "formal("
+            f"{type_map[t]}, {symbolize(n.lower(), opsymbols)}, {has_def}, {is_ex})"
+        )
+    return result
+
 
 def main() -> None:
+    global STACK
     parser = argparse.ArgumentParser()
     parser.add_argument("--opsym-file", dest="opsymbolsFile", required=True)
-    parser.add_argument(
-        "--runtime-base-file", dest="runtimeBaseFile", required=True
-    )
-    parser.add_argument(
-        "--src-template-dir", dest="srcTemplateDir", required=True
-    )
+    parser.add_argument("--runtime-base-file", dest="runtimeBaseFile", required=True)
+    parser.add_argument("--src-template-dir", dest="srcTemplateDir", required=True)
     parser.add_argument("--prefix", dest="prefix", required=True)
     parser.add_argument("--header-out-dir", dest="headerOutDir", required=True)
     parser.add_argument("--src-out-dir", dest="srcOutDir", required=True)
@@ -28,23 +119,6 @@ def main() -> None:
 
     outHeaderFile = os.path.join(headerOutDir, f"{prefix}.h")
     outSrcFile = os.path.join(srcOutDir, f"{prefix}.cc")
-
-    stack = "Stack"
-    errors = 0
-
-    def report_error(filename, line, error):
-        nonlocal errors
-        sys.stderr.write(f"{filename}:{line}: {error}\n")
-        errors = 1
-
-    def assoc_error(filename, line, t):
-        report_error(filename, line, f"no asy type associated to '{t}'")
-
-    def clean_type(v):
-        return re.sub(r"\s+", "", v)
-
-    def clean_params(v: str) -> str:
-        return v.replace("\n", "")
 
     type_map = {}
 
@@ -72,7 +146,7 @@ def main() -> None:
                 l,
             )
             if not match:
-                report_error(filename, line_index, "bad type declaration")
+                Er.report_error(filename, line_index, "bad type declaration")
                 continue
             t, code = match.groups()
             t = clean_type(t)
@@ -87,81 +161,13 @@ def main() -> None:
                 symname, token = m.groups()
                 opsymbols[symname] = token
 
-    def symbolize(name):
-        # If name is alphanumeric/underscore, just SYM(...) it
-        if re.search(r"^[A-Za-z0-9_]+$", name):
-            return f"SYM({name})"
-        # If the name is recognized in opsymbols, substitute
-        if name in opsymbols:
-            return opsymbols[name]
-        # Check if it is "operator smth" form
-        opm = re.search(r"operator\s+(\w+)", name)
-        if opm and opm.group(1) in opsymbols:
-            return opsymbols[opm.group(1)]
-        # Otherwise, generate a trans(...) symbol
-        return f'symbol::trans("{name}")'
-
-    def asy_params(params_string, filename, line):
-        # Split parameters by comma
-        params_list = re.split(r",\s*", params_string) if params_string else []
-        result = []
-        for p in params_list:
-            # (explicit)*\s*(\w*(?:\s*\*)?) => optional 'explicit', type, pointer
-            # (\w*) => parameter name, possibly something like "x"
-            # (=*) => optional equals sign for default parameter
-            match = re.search(
-                r"^\s*(explicit)*\s*"  # optional explicit
-                r"(\w*(?:\s*\*)?)\s*"  # type or pointer type
-                r"(\w*)"  # name
-                r"(=*)",  # if '=' is present, there is a default value
-                p,
-            )
-            if not match:
-                continue
-            explicit, t, n, d = match.groups()
-            t = clean_type(t)
-            if t not in type_map:
-                assoc_error(filename, line, t)
-            has_def = "true" if d else "false"
-            is_ex = "true" if explicit else "false"
-            result.append(
-                f"formal({type_map[t]}, {symbolize(n.lower())}, {has_def}, {is_ex})"
-            )
-        return result
-
-    # Convert parameters into stack pop code in correct order
-    def c_params(params_list):
-        result = []
-        # Reverse order for correct push/pop
-        for p in reversed(params_list):
-            match = re.search(
-                r"^\s*"  # optional whitespace
-                r"(explicit)*"  # optional explicit
-                r"\s*"
-                r"(\w*(?:\s*\*)?)"  # type
-                r"\s*"
-                r"(\w*)"  # parameter name
-                r"(=*)([\w.+\-]*)",  # optional default value
-                p,
-            )
-            if not match:
-                continue
-            _, type_str, name, eqsign, val = match.groups()
-            template = "" if type_str == "item" else f"<{type_str}>"
-            def_val = f",{val}" if eqsign else ""
-            r = f"  {type_str} {name}=vm::pop{template}({stack}{def_val});\n"
-            result.append(r)
-        return result
-
     # Read base, prefix.in, etc., all separated by form-feed + newline
     with open(os.path.join(srcTemplateDir, f"{prefix}.in"), "rb") as f_in:
         # Convert to text with universal newline
         data_in = io.TextIOWrapper(f_in, encoding="utf-8", newline=None).read()
     with open(runtimeBaseFile, "rb") as f_base:
         # Convert to text with universal newline
-        data_base = io.TextIOWrapper(
-            f_base, encoding="utf-8", newline=None
-        ).read()
+        data_base = io.TextIOWrapper(f_base, encoding="utf-8", newline=None).read()
     with open(outSrcFile, "w", newline="", encoding="utf-8") as f_out:
 
         out_autogen = (
@@ -174,8 +180,14 @@ def main() -> None:
         # We split and merge corresponding sections delineated by form-feed
         sections_base = data_base.split("\f\n")
         sections_base[:-1] = [section + "\f\n" for section in sections_base[:-1]]
+        # For now, imitate the behavior of the original perl code: if the last section
+        # is empty, remove it.
+        if sections_base and not sections_base[-1]:
+            sections_base.pop()
         sections_in = data_in.split("\f\n")
         sections_in[:-1] = [section + "\f\n" for section in sections_in[:-1]]
+        if sections_in and not sections_in[-1]:
+            sections_in.pop()
 
         # Attempt to replicate the same line counting logic
         # Just track how many lines have passed
@@ -183,9 +195,7 @@ def main() -> None:
         in_line_counter = 1
 
         # 1) runtimebase.in chunk #1
-        f_out.write(
-            f'#line {base_line_counter} "{srcTemplateDir}/runtimebase.in"\n'
-        )
+        f_out.write(f'#line {base_line_counter} "{srcTemplateDir}/runtimebase.in"\n')
         baseheader = sections_base[0]
         f_out.write(baseheader)
         base_line_counter += baseheader.count("\n")
@@ -193,9 +203,7 @@ def main() -> None:
         base_start_type = base_line_counter
 
         # 2) prefix.in chunk #1
-        f_out.write(
-            f'#line {in_line_counter} "{srcTemplateDir}/{prefix}.in"\n'
-        )
+        f_out.write(f'#line {in_line_counter} "{srcTemplateDir}/{prefix}.in"\n')
         header = sections_in[0]
         f_out.write(header)
         in_line_counter += header.count("\n")
@@ -210,9 +218,7 @@ def main() -> None:
         in_line_counter += types.count("\n")
 
         # 4) next chunk from base
-        f_out.write(
-            f'#line {base_line_counter} "{srcTemplateDir}/runtimebase.in"\n'
-        )
+        f_out.write(f'#line {base_line_counter} "{srcTemplateDir}/runtimebase.in"\n')
         baseheader2 = sections_base[2] if len(sections_base) > 2 else ""
         f_out.write(baseheader2)
         base_line_counter += baseheader2.count("\n")
@@ -263,7 +269,7 @@ def main() -> None:
             next_in_line_counter = in_line_counter + section.count("\n")
             md = pat.search(section)
             if not md:
-                report_error(
+                Er.report_error(
                     f"{prefix}.in", in_line_counter, "bad function definition"
                 )
                 continue
@@ -288,9 +294,13 @@ def main() -> None:
                     r"Operator\s*", "", name
                 )  # If there's "Operator", remove it
                 if type_ not in type_map:
-                    assoc_error(f"{prefix}.in", in_line_counter, type_)
+                    Er.assoc_error(f"{prefix}.in", in_line_counter, type_)
                 asy_param_list = asy_params(
-                    params, f"{prefix}.in", in_line_counter
+                    params,
+                    f"{prefix}.in",
+                    in_line_counter,
+                    type_map,
+                    opsymbols,
                 )
                 asy_params_comma = ""
                 if asy_param_list:
@@ -299,7 +309,7 @@ def main() -> None:
                 builtin.append(
                     f'#line {in_line_counter} "{srcTemplateDir}/{prefix}.in"\n'
                     f"  addFunc(ve, run::{cname}, {type_map[type_]}, "
-                    f"{symbolize(name)}{asy_params_comma});\n"
+                    f"{symbolize(name,opsymbols)}{asy_params_comma});\n"
                 )
             else:
                 # Note: earlier code ensures cname is not empty.
@@ -313,7 +323,7 @@ def main() -> None:
             qualifier = "" if type_ == "item" else f"<{type_}>"
             code_transformed = re.sub(
                 r"\breturn\s+([^;]*);",  # read until the next semicolon
-                rf"{{{stack}->push{qualifier}(\1); return;}}",
+                rf"{{{STACK}->push{qualifier}(\1); return;}}",
                 code,
             )
 
@@ -323,9 +333,7 @@ def main() -> None:
             # Write out any preceding comments
             f_out.write(comments)
             in_line_counter += comments.count("\n")
-            f_out.write(
-                f'#line {in_line_counter} "{srcTemplateDir}/{prefix}.in"\n'
-            )
+            f_out.write(f'#line {in_line_counter} "{srcTemplateDir}/{prefix}.in"\n')
 
             # Write out the function prototype as a comment
             prototype = f"{type_} {name}({params});"
@@ -336,14 +344,12 @@ def main() -> None:
                 f_out.write(f"// {prototype}\n")
 
             # Actual function definition in prefix.cc
-            param_name = stack if type_ != "void" or param_list else ""
+            param_name = STACK if type_ != "void" or param_list else ""
             f_out.write(f"void {cname}(stack *{param_name})\n")
             f_out.write("{\n")
             assert not param_code or param_code[-1] == "\n"
             f_out.write(param_code)
-            f_out.write(
-                f'#line {in_line_counter} "{srcTemplateDir}/{prefix}.in"'
-            )
+            f_out.write(f'#line {in_line_counter} "{srcTemplateDir}/{prefix}.in"')
             assert code_transformed[0] == "\n"
             f_out.write(code_transformed)
             f_out.write("}\n\n")
@@ -370,7 +376,7 @@ def main() -> None:
         with open(outHeaderFile, "w", encoding="utf-8") as hf:
             hf.write(new_header)
 
-    if errors:
+    if Er.errors:
         try:
             os.unlink(outHeaderFile)
         except FileNotFoundError:
@@ -379,7 +385,7 @@ def main() -> None:
             os.unlink(outSrcFile)
         except FileNotFoundError:
             pass
-        sys.exit(errors)
+        sys.exit(Er.errors)
 
 
 if __name__ == "__main__":
