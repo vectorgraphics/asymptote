35739e8997ab683e327b8bdbfbc8008f76a98aca
Charles Staats III <charles.staats.iii@gmail.com>
2025-01-02 18:53:27 -0800

    BROKEN: Working toward better implementation for templated imports.

--- genv.h
+++ genv.h
@@ -21,7 +21,6 @@
 #include "record.h"
 #include "absyn.h"
 #include "access.h"
-#include "coenv.h"
 #include "stack.h"
 
 using types::record;
@@ -47,8 +46,7 @@ class genv : public gc {
   record *loadTemplatedModule(
       symbol id,
       string filename,
-      mem::vector<absyntax::namedTyEntry*> *args,
-      coenv& e
+      mem::vector<absyntax::namedTyEntry*> *args
   );
 
 public:
@@ -59,8 +57,7 @@ public:
   record *getTemplatedModule(
       symbol index,
       string filename,
-      mem::vector<absyntax::namedTyEntry*> *args,
-      coenv& e
+      mem::vector<absyntax::namedTyEntry*> *args
   );
   record *getLoadedModule(symbol index);
 
@@ -68,44 +65,6 @@ public:
   // used at runtime.
   vm::stack::importInitMap *getInitMap();
 };
-
-/* Plan for implementation of templated modules:
- *    
- *    Translating an access declaration:
- *    
- *    access Map(Key=A, Value=B) as MapAB;
- *    
- *    run encodeLevel for both A and B
- *    this should give the parent records for each struct
- *    encode pushing the string "Map/1234567" on the stack
- *    encode call to builtin loadTemplatedModule
- *    also save into MapAB (varinit)
- *    
- *    build list of types (or tyEntry?)
- *    
- *    also ensure names match
- *    
- *    *****
- *    
- *    At runtime, loadTemplatedModule pops the string
- *    
- *    if the module is already loaded, it pops the levels
- *    and returns the already loaded module.
- *    
- *    if the module is not loaded, it leaves the levels on the stack
- *    and calls the initializer for the templated module
- *    
- *    it might be easiest to give the number of pushed params as an argument
- *    to loadTemplatedModule (ints and strings have no push/pop)
- *    
- *    *****
- *    
- *    Translating a templated module
- *    
- *    we start translating a file with a list of (name, tyEntry)
- *    or (name, type) pairs
- *    
- */
 
 } // namespace trans
 
