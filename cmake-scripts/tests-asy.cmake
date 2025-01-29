82a7653b9f43f33becd4dce3f5aa144f623830fe
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2025-01-28 23:01:19 -0700

    CMAKE: Add check labels to asymptote tests.

--- cmake-scripts/tests-asy.cmake
+++ cmake-scripts/tests-asy.cmake
@@ -6,14 +6,15 @@ set(ASY_ASYLANG_TEST_SCRATCH_DIR ${ASY_ASYLANG_TEST_ROOT}/out/)
 function(add_individual_asy_tests)
     set(fn_opts)
     set(fn_oneval_args DIR FILE ADDR_ASY_ARGS)
-    set(fn_multival_args)
+    set(fn_multival_args TEST_LABELS)
     cmake_parse_arguments(
             ASY_TEST "${fn_opts}" "${fn_oneval_args}" "${fn_multival_args}" ${ARGN}
     )
 
     set(TEST_PATH ${ASY_ASYLANG_TEST_ROOT}/${ASY_TEST_DIR}/${ASY_TEST_FILE}.asy)
+    set(TEST_NAME "asy.${ASY_TEST_DIR}.${ASY_TEST_FILE}")
     add_test(
-            NAME "asy.${ASY_TEST_DIR}.${ASY_TEST_FILE}"
+            NAME ${TEST_NAME}
             COMMAND asy
                 -dir ${ASY_BUILD_BASE_DIR} ${TEST_PATH}
                 -noV
@@ -22,20 +23,37 @@ function(add_individual_asy_tests)
                 ${ASY_TEST_ADDR_ASY_ARGS}
             WORKING_DIRECTORY ${ASY_ASYLANG_TEST_ROOT}
     )
+
+    if (ASY_TEST_TEST_LABELS)
+        set_property(
+                TEST ${TEST_NAME}
+                PROPERTY LABELS ${ASY_TEST_TEST_LABELS}
+        )
+    endif()
+
 endfunction()
 
 macro(add_asy_tests)
-    set(macro_opts)
+    set(macro_opts TEST_NOT_PART_OF_CHECK_TEST)
     set(macro_oneval_args TEST_DIR ADDR_ASY_ARGS)
     set(macro_multival_args TESTS TEST_ARTIFACTS)
     cmake_parse_arguments(
             ASY_TESTING "${macro_opts}" "${macro_oneval_args}" "${macro_multival_args}" ${ARGN}
     )
     foreach(testfile ${ASY_TESTING_TESTS})
+
+        if (ASY_TESTING_TEST_NOT_PART_OF_CHECK_TEST)
+            set(TEST_LABEL asy-extended-tests)
+        else()
+            set(TEST_LABEL asy-check-tests)
+        endif()
+
+
         add_individual_asy_tests(
                 DIR ${ASY_TESTING_TEST_DIR}
                 FILE ${testfile}
                 ADDR_ASY_ARGS ${ASY_TESTING_ADDR_ASY_ARGS}
+                TEST_LABELS ${TEST_LABEL}
         )
     endforeach()
 
@@ -68,6 +86,7 @@ if (ENABLE_GC)
             TEST_DIR gc
             TESTS array funcall guide label path shipout string struct transform
             TEST_ARTIFACTS .eps
+            TEST_NOT_PART_OF_CHECK_TEST true
     )
 endif()
 
@@ -103,4 +122,5 @@ add_asy_tests(
 add_asy_tests(
         TEST_DIR gs
         TESTS ghostscript
+        TEST_NOT_PART_OF_CHECK_TEST true
 )
