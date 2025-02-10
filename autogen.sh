214552f58197224a5b3a7efd4a15a6757c90813b
John Bowman <bowman@ualberta.ca>
2025-02-10 13:35:08 -0800

    Remove libtool dependence from configure.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+command -v libtoolize >/dev/null 2>&1
+if  [ $? -ne 0 ]; then
+    command -v libtool >/dev/null 2>&1
+    if  [ $? -ne 0 ]; then
+        echo "autogen.sh: error: could not find libtool.  libtool is required to run autogen.sh." 1>&2
+        exit 1
+    fi
+fi
+
 autoheader && autoconf
 
 cd gc && ./autogen.sh
