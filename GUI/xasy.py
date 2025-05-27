fdafd8684d31f1953b4aa58403af3e7c28732590
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2025-05-26 23:43:16 -0600

    QT6: Remove high DPI scaling options.

--- GUI/xasy.py
+++ GUI/xasy.py
@@ -7,13 +7,10 @@ sys.path.append(str(pathlib.Path(__file__).resolve().parent))
 
 import signal, os
 import PySide6.QtWidgets as QtWidgets
-import PySide6.QtCore as QtCore
 from Window1 import MainWindow1
 
 def main(args):
     os.environ["QT_LOGGING_RULES"]="*.debug=false;qt.qpa.*=false"
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps,True)
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling,True)
     qtApp = QtWidgets.QApplication(args)
     signal.signal(signal.SIGINT,signal.SIG_DFL)
     mainWin1 = MainWindow1()
