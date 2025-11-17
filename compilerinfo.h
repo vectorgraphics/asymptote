a020257a6df2b1e485860ea53487a39941acfb98
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2025-11-16 22:59:45 -0700

    CXX: Add ASY_COMPILER_MACRO compiler information string.

---
+++ compilerinfo.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+
+#if defined(_MSC_VER)
+#  if defined(_M_X64)
+std::string const ARCH_STRING= "x64";
+#  elif defined(_M_ARM)
+std::string const ARCH_STRING= "ARM";
+#  elif defined(_M_IX86)
+std::string const ARCH_STRING= "x86";// is this macro even needed?
+#  elif defined(_M_ARM64)
+std::string const ARCH_STRING= "ARM64";
+#  else
+std::string const ARCH_STRING= "Unknown architecture";
+#  endif
+std::string const ASY_COMPILER_INFO=
+        "MSVC v" + std::to_string(_MSC_VER) + " (" + ARCH_STRING + ")";
+#elif defined(__GNUC__)// gcc-class compilers
+
+#  if defined(__x86_64__)
+std::string const ARCH_STRING= "x64";
+#  elif defined(__arm__)
+std::string const ARCH_STRING= "ARM";
+#  elif defined(__aarch64__)
+std::string const ARCH_STRING= "ARM64";
+#  else
+std::string const ARCH_STRING= "Unknown architecture";
+#  endif
+
+#  if defined(__clang__)// for clang
+std::string const ASY_COMPILER_INFO=
+        "clang " + __clang_version__ + " (" + ARCH_STRING + ")";
+#  else                 // for gcc
+std::string const ASY_COMPILER_INFO= "GCC " + std::to_string(__GNUC__) + "." +
+                                     std::to_string(____GNUC_MINOR__) + "." +
+                                     std::to_string(__GNUC_PATCHLEVEL__) +
+                                     " (" + ARCH_STRING + ")";
+#  endif
+
+
+#else
+const std::string ASY_COMPILER_INFO= "unknown compiler"
+#endif
