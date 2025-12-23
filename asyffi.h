1399ac77dfb6735b0087a776be031ddfa44c12f7
Jamie Rassmaeemasmuang <jamievlin@outlook.com>
2025-12-22 21:07:09 -0700

    FFI: Add LNK_CALL and message on apple/systems not windows or linux.

--- asyffi.h
+++ asyffi.h
@@ -7,11 +7,36 @@
 
 #include <cstdint>
 
-#ifdef _WIN32
+#if defined(_WIN32)
 #  define ASY_FFI_EXPORT __declspec(dllexport)
+
+// In case someone targets 32-bit x86 systems, this calling convention is
+// needed
+#ifndef _M_X64
+#define LNK_CALL __cdecl
 #else
-#include <cstddef>
+#define LNK_CALL
+#endif
+
+#elif defined(__linux__)
+#  include <cstddef>
 #  define ASY_FFI_EXPORT [[gnu::visibility("default")]]
+
+// like the case for windows. LNK_CALL is not used for 64-bit systems
+#ifndef __LP64__
+#define LNK_CALL __attribute__((__cdecl__))
+#else
+#define LNK_CALL
+#endif
+
+#elif defined(__APPLE__)
+
+// As Jamie does not have a Mac machine, if FFI is to be supported on macs,
+// someone else has to implement it ( unfortunately :( )
+#error FFI is not yet ready for mac-based systems. \
+  Additionally, work has to be done for ARM vs x86 systems
+#else
+#error Right now, FFI is only supported for windows and linux systems
 #endif
 
 class IAsyItem
