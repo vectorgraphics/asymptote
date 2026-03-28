1b3df82b0ce171c17c341e74dd203d20334e0128
Charles <charles.staats.iii@gmail.com>
2026-03-27 19:39:35 -0700

    Minor fixes to random.h.

--- random.h
+++ random.h
@@ -1,4 +1,8 @@
 
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <cstdint>
 #include <random>
 #include <string>
 
@@ -7,3 +11,5 @@ void seed(int64_t seed);
 int64_t randInt(int64_t min, int64_t max);
 double unitrand();
 }  // namespace camp
+
+#endif
