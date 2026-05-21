ec0effb6e52a6d6d722ee2cd08d2f6272c052bdb
LLLM (Linear-in-time LLM coding assistant) <bowman@ualberta.ca>
2026-05-21 10:56:11 -0700

    Move AsyGLRender and AsyVkRender members from public to private, removing dead friend declarations

--- vkrender.h
+++ vkrender.h
@@ -98,23 +98,31 @@ public:
   void onWindowFocus(int focused) override;
   void onClose() override;
 
+  // Virtual overrides called by base class
+  void updateHandler(int=0) override;
+  void drawFrame() override;
+  void swapBuffers() override;
+  void Export(int imageIndex=0) override;
+  void finalizeProcess() override;
+
+  GLFWwindow* getRenderWindow() const;
+  GLFWwindow* getGLFWWindow() const { return glfwWindow; }
+
+private:
   bool framebufferResized=false;
   bool recreatePipeline=false;
   bool recreateBlendPipeline=false;
   bool shouldUpdateBuffers=true;
   bool newUniformBuffer=true;
-  // Note: ibl is now in base class AsyRender
   bool vkexit=false;
+  bool readyForUpdate=false;
 
   int maxFramesInFlight;
-
   vk::SampleCountFlagBits samples = vk::SampleCountFlagBits::e1;
-
   std::uint32_t pixels;
-
   const double* dprojView;
   const double* dView;
-private:
+
   static constexpr std::array<const char*, 4> deviceExtensions = {
     VK_KHR_DEPTH_STENCIL_RESOLVE_EXTENSION_NAME,
     VK_KHR_CREATE_RENDERPASS_2_EXTENSION_NAME,
@@ -396,10 +404,11 @@ private:
 protected:
   void updateModelViewData() override;
   void setProjection() override;
+  void exportHandler(int) override;
+  virtual void reshape(int width, int height) override;
+  virtual void cycleMode() override;
 
-public:
-  void updateHandler(int=0) override;
-
+private:
   void initWindow();
   void initVulkan();
 
@@ -600,20 +609,11 @@ public:
   void drawBuffers(FrameObject & object, int imageIndex);
   void beginTransferRecording(FrameObject & object);
   void endAndSubmitTransfers(FrameObject & object, vk::Queue queue);
-  void drawFrame() override;
-  void swapBuffers() override;
   void recreateSwapChain();
   void initializeSwapChainIfNeeded();
   vk::UniqueShaderModule createShaderModule(EShLanguage lang, std::string const & filename, std::vector<std::string> const & options);
 
-  GLFWwindow* getRenderWindow() const;
   void cleanup();
-
-  // user controls
-  void exportHandler(int) override;
-  void Export(int imageIndex=0) override;
-  bool readyForUpdate=false;
-  // Note: initialized and format3dWait are now in base class AsyRender
 
   struct PipelineConfig {
     vk::PrimitiveTopology topology;
@@ -639,19 +639,6 @@ public:
     PipelineType start = PIPELINE_OPAQUE,
     PipelineType end = PIPELINE_MAX
   );
-
-  // Graphics library cleanup
-  void finalizeProcess() override;
-
-  /** Returns the GLFW window pointer */
-  GLFWwindow* getGLFWWindow() const { return glfwWindow; }
-
-  // Vulkan-specific overrides that add to base class behavior
-  virtual void reshape(int width, int height) override;
-  virtual void cycleMode() override;
-
-  friend void glfwInitWindow(AsyRender*, int, int, const std::string&);
-  friend void glfwCleanupWindow(AsyVkRender*);
 };
 
 } // namespace camp
