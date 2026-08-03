96a6d63a024a68a826eb911436e5f61baf873524
LIM Coding Assistant https://github.com/statefullm/lim <bowman@ualberta.ca>
2026-08-02 20:54:26 -0700

    Add gl-matrix to license infrastructure, remove embedded license headers from asygl.js, and add terser post-minification step.

--- cmake-scripts/copy-build-licenses.cmake
+++ cmake-scripts/copy-build-licenses.cmake
@@ -35,5 +35,6 @@ asy_copy_build_license(libatomic_ops/LICENSE        libatomic_ops-LICENSE.txt)
 asy_copy_build_license(libatomic_ops/COPYING        libatomic_ops-COPYING.txt)
 asy_copy_build_license(tinyexr/LICENSE.txt          tinyexr-LICENSE.txt)
 asy_copy_build_license(gc/LICENSE.txt               gc-LICENSE.txt)
+asy_copy_build_license(webgl/gl-matrix-license      gl-matrix-LICENSE.md)
 
 add_custom_target(asy-licenses DEPENDS ${ASY_BUILD_LICENSE_COPIES})
