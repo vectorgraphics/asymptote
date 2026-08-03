7b1ba09fef03796649430c1d9ff8025a5a8d7197
Jamie Lindner <jamievlin@outlook.com>
2026-08-03 17:30:37 -0600

    HASH: Fix comments about equivalent python code.

--- symbol.cc
+++ symbol.cc
@@ -114,16 +114,16 @@ uint hash(const char *s, size_t len)
 
 def hash(s: str) -> int:
   h = ord(s[0])
-  if len(s) == 2:
+  if len(s) == 1:
     return h
   h += 25191 * ord(s[1])
-  if len(s) == 3:
+  if len(s) == 2:
     return h;
   h += 16342 * ord(s[2])
-  if len(s) == 4:
+  if len(s) == 3:
     return h;
   h += 1746 * ord(s[3])
-  if len(s) == 5:
+  if len(s) == 4:
     return h;
   return h + (18326 * ord(s[4])) + len(s)
 
